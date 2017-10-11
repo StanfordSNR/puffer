7b7707ef2a42979e80b0527ae01156cd0d0aa9af
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-11 12:58:46 -0700

    strict_conversions: add narrow_cast and strict_atoui

--- src/util/strict_conversions.hh
+++ src/util/strict_conversions.hh
@@ -3,9 +3,23 @@
 #ifndef STRICT_CONVERSIONS_HH
 #define STRICT_CONVERSIONS_HH
 
+#include <stdexcept>
 #include <string>
 
 long int strict_atoi( const std::string & str, const int base = 10 );
 double strict_atof( const std::string & str );
+unsigned long int strict_atoui(const std::string & str, const int base = 10);
+
+template<class Target, class Source>
+Target narrow_cast(const Source & v)
+{
+  auto r = static_cast<Target>(v);
+
+  if (static_cast<Source>(r) != v) {
+    throw std::runtime_error("narrow_cast failed");
+  }
+
+  return r;
+}
 
 #endif /* STRICT_CONVERSIONS_HH */
