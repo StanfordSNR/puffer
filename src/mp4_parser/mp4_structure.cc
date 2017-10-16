edd13d024f595601b8be83b0258b4b07d3cee619
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-16 23:45:00 +0000

    Add placeholder files to parse MP4 structure

---
+++ src/mp4_parser/mp4_structure.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include "mp4_parser.hh"
+
+using namespace std;
+
+void print_usage(const string & program_name)
+{
+  cerr << "Usage: " << program_name << " <file.mp4>" << endl
+       << endl
+       << "<file.mp4>        " << "MP4 file to parse" << endl;
+}
+
+int main(int argc, char * argv[])
+{
+  if (argc < 1) {
+    abort();
+  }
+
+  if (argc != 2) {
+    print_usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  auto parser = make_unique<MP4::Parser>(argv[1]);
+
+  return EXIT_SUCCESS;
+}
