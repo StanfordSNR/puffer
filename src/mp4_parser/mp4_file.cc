f4497deaf760d4c2e48a223dfd31e1232f5060ef
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-17 02:37:08 +0000

    mp4_box / mp4_file: necessary functions to print architecture

--- src/mp4_parser/mp4_file.cc
+++ src/mp4_parser/mp4_file.cc
@@ -1,7 +1,7 @@
 #include <fcntl.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <iostream>
+#include <endian.h>
 
 #include "exception.hh"
 #include "strict_conversions.hh"
@@ -15,15 +15,39 @@ MP4File::MP4File(const string & filename)
                                    open(filename.c_str(), O_RDONLY, 0)))
 {}
 
-int MP4File::curr_offset()
+inline int64_t MP4File::seek(int64_t offset, int whence)
 {
-  off_t curr_pos = CheckSystemCall("lseek", lseek(fd_num(), 0, SEEK_CUR));
-  return narrow_cast<int>(curr_pos);
+  return CheckSystemCall("lseek", lseek(fd_num(), offset, whence));
+}
+
+int64_t MP4File::curr_offset()
+{
+  return seek(0, SEEK_CUR);
+}
+
+int64_t MP4File::inc_offset(int64_t offset)
+{
+  return seek(offset, SEEK_CUR);
+}
+
+int64_t MP4File::filesize()
+{
+  int64_t prev_offset = curr_offset();
+  int64_t fsize = seek(0, SEEK_END); /* seek to end of file */
+  seek(prev_offset, SEEK_SET); /* seek back to previous offset */
+  return fsize;
 }
 
 uint32_t MP4File::read_uint32()
 {
   string data = read(4);
   const uint32_t * size = reinterpret_cast<const uint32_t *>(data.c_str());
-  return ntohl(*size);
+  return be32toh(*size);
+}
+
+uint64_t MP4File::read_uint64()
+{
+  string data = read(8);
+  const uint64_t * size = reinterpret_cast<const uint64_t *>(data.c_str());
+  return be64toh(*size);
 }
