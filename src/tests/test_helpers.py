237946b69822eb93ef1af6d7f13f31d68a7b95ea
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-22 00:27:19 -0700

    Add tests for run_notifier

---
+++ src/tests/test_helpers.py
@@ -0,0 +1,35 @@
+import sys
+import subprocess
+from subprocess import PIPE
+
+
+def print_cmd(cmd):
+    if isinstance(cmd, list):
+        cmd_to_print = ' '.join(cmd).strip()
+    elif isinstance(cmd, (str, unicode)):
+        cmd_to_print = cmd.strip()
+    else:
+        cmd_to_print = ''
+
+    if cmd_to_print:
+        sys.stderr.write('$ %s\n' % cmd_to_print)
+
+
+def call(cmd, **kwargs):
+    print_cmd(cmd)
+    return subprocess.call(cmd, **kwargs)
+
+
+def check_call(cmd, **kwargs):
+    print_cmd(cmd)
+    return subprocess.check_call(cmd, **kwargs)
+
+
+def check_output(cmd, **kwargs):
+    print_cmd(cmd)
+    return subprocess.check_output(cmd, **kwargs)
+
+
+def Popen(cmd, **kwargs):
+    print_cmd(cmd)
+    return subprocess.Popen(cmd, **kwargs)
