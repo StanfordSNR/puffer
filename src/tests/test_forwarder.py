e998997098f591823bfbbf576713f690e1de0ff5
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-22 02:41:52 -0700

    test_forwarder.py: use sleep to allow socket to set up for now

--- src/tests/test_forwarder.py
+++ src/tests/test_forwarder.py
@@ -4,6 +4,7 @@ import os
 from os import path
 import random
 import string
+from time import sleep
 from test_helpers import get_open_port, Popen, check_call, check_output
 
 
@@ -23,6 +24,7 @@ def main():
     # run TCP receiver
     tcp_port = get_open_port()
     tcp_proc = Popen('nc -l %s > %s' % (tcp_port, copy_file), shell=True)
+    sleep(0.5)
 
     # run forwarder
     forwarder = path.abspath(
@@ -31,10 +33,12 @@ def main():
     udp_port = get_open_port()
     forwarder_proc = Popen([forwarder, '--udp-port', udp_port,
                             '--tcp', '127.0.0.1:' + tcp_port])
+    sleep(0.5)
 
-    # run UDP sender, which will exit 1 sec after completing the file transfer
-    check_call('nc -w 1 -u 127.0.0.1 %s < %s' %
+    # run UDP sender, which will after completing the file transfer
+    check_call('nc -w 0 -u 127.0.0.1 %s < %s' %
                (udp_port, orig_file), shell=True)
+    sleep(0.5)
 
     forwarder_proc.terminate()
     tcp_proc.terminate()
