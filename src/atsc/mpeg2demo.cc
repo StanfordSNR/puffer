38afbe115e402a7b41a4507a53f966695bafefa9
Keith Winstein <keithw@cs.stanford.edu>
2017-10-23 13:07:35 -0700

    mpeg2demo: move beginnings of TS parsing to a class

--- src/atsc/mpeg2demo.cc
+++ src/atsc/mpeg2demo.cc
@@ -2,14 +2,49 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <string_view>
 
 #include "file_descriptor.hh"
 #include "exception.hh"
 
 using namespace std;
+
 const size_t ts_packet_length = 188;
 const char ts_packet_sync_byte = 0x47;
 const size_t packets_in_chunk = 512;
+
+class TSParser
+{
+private:
+  unsigned int pid_; /* program ID of interest */
+
+  unsigned int packets_parsed_ {}; /* count of TS packets successfully parsed */
+
+public:
+  TSParser( const unsigned int pid )
+    : pid_( pid )
+  {
+    if ( pid >= (1 << 13) ) {
+      throw runtime_error( "program ID must be less than " + to_string( 1 << 13 ) );
+    }
+  }
+
+  void parse( const string_view & packet )
+  {
+    if ( packet.length() != ts_packet_length ) {
+      throw runtime_error( "invalid TS packet length" );
+    }
+
+    if ( packet.front() != ts_packet_sync_byte ) {
+      throw runtime_error( "invalid TS sync byte" );
+    }
+
+    packets_parsed_++;
+  }
+
+  /* accessors */
+  unsigned int packets_parsed() const { return packets_parsed_; }
+};
 
 int main( int argc, char *argv[] )
 {
@@ -24,7 +59,7 @@ int main( int argc, char *argv[] )
 
   FileDescriptor stdin { 0 };
 
-  unsigned int packets_parsed = 0;
+  TSParser parser { pid };
   int ret = EXIT_SUCCESS;
 
   try {
@@ -32,12 +67,11 @@ int main( int argc, char *argv[] )
       /* read chunks of MPEG-2 transport-stream packets in a loop */
 
       const string chunk = stdin.read_exactly( ts_packet_length * packets_in_chunk );
-      for ( unsigned packet_no = 0; packet_no < packets_in_chunk; packet_no++ ) {
-        if ( chunk[ packet_no * ts_packet_length ] != ts_packet_sync_byte ) {
-          throw runtime_error( "invalid TS sync byte" );
-        }
+      const string_view chunk_view { chunk.data(), chunk.size() };
 
-        packets_parsed++;
+      for ( unsigned packet_no = 0; packet_no < packets_in_chunk; packet_no++ ) {
+        parser.parse( chunk_view.substr( packet_no * ts_packet_length,
+                                         ts_packet_length ) );
       }
     }
   } catch ( const exception & e ) {
@@ -45,7 +79,7 @@ int main( int argc, char *argv[] )
     ret = EXIT_FAILURE;
   }
 
-  cerr << "Packets successfully parsed: " << packets_parsed << "\n";
+  cerr << "Packets successfully parsed: " << parser.packets_parsed() << "\n";
 
   return ret;
 }
