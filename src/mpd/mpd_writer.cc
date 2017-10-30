88c1ef48612bb953a4af115cac508c58cafa3620
Keyi Zhang <keyi@stanford.edu>
2017-10-29 17:23:41 -0700

    Clean up mpd.xx. Add init.mp4 and m4s reading to mpd_writer

--- src/mpd/mpd_writer.cc
+++ src/mpd/mpd_writer.cc
@@ -5,10 +5,11 @@
 #include "mpd.hh"
 #include "path.hh"
 #include "mp4_info.hh"
+#include "mp4_parser.hh"
 
 using namespace std;
 using namespace MPD;
-
+using namespace MP4;
 
 const char *optstring = "u:p:b:s:i:";
 const struct option options[] = {
@@ -36,6 +37,49 @@ void print_usage(const string & program_name)
        << "-s --segment-name <name>     Set the segment name template." << endl
        << "-i --init-name <name>        Set the initial segment name." << endl
        << endl;
+}
+
+void add_representation(shared_ptr<VideoAdaptionSet> v_set,
+    shared_ptr<AudioAdaptionSet> a_set, const string & init,
+        const string & segment)
+{
+  /* load mp4 up using parser */
+  auto i_parser = make_shared<MP4Parser>(init);
+  auto s_parser = make_shared<MP4Parser>(segment);
+  auto i_info = MP4Info(i_parser);
+  auto s_info = MP4Info(s_parser);
+  /* find duration, timescale from init and segment individually */
+  uint32_t i_duration, s_duration, i_timescale, s_timescale;
+  tie(i_timescale, i_duration) = i_info.get_timescale_duration();
+  tie(s_timescale, s_duration) = s_info.get_timescale_duration();
+  /* selecting the proper values because mp4 atoms are a mess */
+  uint32_t duration = s_duration;
+  /* override the timescale from init.mp4 */
+  uint32_t timescale = s_timescale == 0? i_timescale : s_timescale;
+  if ( duration == 0) {
+    throw runtime_error("Cannot find duration in " + segment);
+  }
+  /* get bitrate */
+  uint32_t bitrate = s_info.get_bitrate();
+  /* get fps */
+  if (i_info.is_video()) {
+    /* this is a video */
+    uint16_t width, height;
+    uint8_t profile, avc_level;
+    tie(width, height) = i_info.get_width_height();
+    tie(profile, avc_level) = i_info.get_avc_profile_level();
+    float fps = s_info.get_fps();
+    /* id will be set later */
+    auto repr_v = make_shared<VideoRepresentation>(
+        "", width, height, bitrate, profile, avc_level, fps, timescale);
+    v_set->add_repr(repr_v);
+  } else {
+    /* this is an audio */
+    /* TODO: add audio support */
+    auto repr_a = make_shared<AudioRepresentation>("1", 100000, 180000, true,
+        timescale);
+    a_set->add_repr(repr_a);
+  }
 }
 
 int main(int argc, char * argv[])
@@ -93,11 +137,11 @@ int main(int argc, char * argv[])
   }
 
   auto set_v = make_shared<VideoAdaptionSet>(1, "test1", "test2", 23.976,
-          240, 100);
-  auto set_a = make_shared<AudioAdaptionSet>(2, "test1", "test2", 240, 100);
+          240);
+  auto set_a = make_shared<AudioAdaptionSet>(2, "test1", "test2", 240);
   auto repr_v = make_shared<VideoRepresentation>(
-    "1", 800, 600, 100000, 100, 20, 23.976);
-  auto repr_a = make_shared<AudioRepresentation>("1", 100000, 180000, true);
+    "1", 800, 600, 100000, 100, 20, 23.976, 100);
+  auto repr_a = make_shared<AudioRepresentation>("1", 100000, 180000, false, 100);
   set_v->add_repr(repr_v);
 
   w->add_video_adaption_set(set_v);
