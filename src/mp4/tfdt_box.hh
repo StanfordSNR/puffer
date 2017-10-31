30ec1c3a70901863ac2c3d7e4fa0c454d8c0c6ee
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-31 12:54:41 -0700

    Add write_box to tfdt_box.xx

--- src/mp4/tfdt_box.hh
+++ src/mp4/tfdt_box.hh
@@ -9,6 +9,10 @@ class TfdtBox : public FullBox
 {
 public:
   TfdtBox(const uint64_t size, const std::string & type);
+  TfdtBox(const std::string & type,
+          const uint8_t version,
+          const uint32_t flags,
+          const uint64_t base_media_decode_time);
 
   /* accessors */
   uint64_t base_media_decode_time() { return base_media_decode_time_; }
@@ -16,6 +20,7 @@ public:
   void print_structure(const unsigned int indent = 0);
 
   void parse_data(MP4File & mp4, const uint64_t data_size);
+  void write_box(MP4File & mp4);
 
 private:
   uint64_t base_media_decode_time_;
