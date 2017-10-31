9d441a1c6ea67287a379b70f68989d1e6fd5571c
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-31 12:54:41 -0700

    Add write_box to tfdt_box.xx

--- src/mp4/tfdt_box.cc
+++ src/mp4/tfdt_box.cc
@@ -1,12 +1,21 @@
 #include <iostream>
 
 #include "tfdt_box.hh"
+#include "strict_conversions.hh"
 
 using namespace std;
 using namespace MP4;
 
 TfdtBox::TfdtBox(const uint64_t size, const std::string & type)
   : FullBox(size, type), base_media_decode_time_()
+{}
+
+TfdtBox::TfdtBox(const std::string & type,
+                 const uint8_t version,
+                 const uint32_t flags,
+                 const uint64_t base_media_decode_time)
+  : FullBox(type, version, flags),
+    base_media_decode_time_(base_media_decode_time)
 {}
 
 void TfdtBox::print_structure(const unsigned int indent)
@@ -30,4 +39,20 @@ void TfdtBox::parse_data(MP4File & mp4, const uint64_t data_size)
   }
 
   check_data_left(mp4, init_offset, data_size);
+}
+
+void TfdtBox::write_box(MP4File & mp4)
+{
+  uint64_t size_offset = mp4.curr_offset();
+
+  write_size_type(mp4);
+  write_version_flags(mp4);
+
+  if (version() == 1) {
+    mp4.write_uint64(base_media_decode_time_);
+  } else {
+    mp4.write_uint32(narrow_cast<uint32_t>(base_media_decode_time_));
+  }
+
+  fix_size_at(mp4, size_offset);
 }
