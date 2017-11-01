3a13577e131950b7e8a353258497e5c4566daa6a
Francis Y. Yan <fyy@cs.stanford.edu>
2017-11-01 00:31:21 -0700

    Add write_box to mdhd box

--- src/mp4/mdhd_box.cc
+++ src/mp4/mdhd_box.cc
@@ -10,6 +10,19 @@ MdhdBox::MdhdBox(const uint64_t size, const string & type)
     timescale_(), duration_(), language_()
 {}
 
+MdhdBox::MdhdBox(const string & type,
+                 const uint8_t version,
+                 const uint32_t flags,
+                 const uint64_t creation_time,
+                 const uint64_t modification_time,
+                 const uint32_t timescale,
+                 const uint64_t duration,
+                 const uint16_t language)
+  : FullBox(type, version, flags), creation_time_(creation_time),
+    modification_time_(modification_time), timescale_(timescale),
+    duration_(duration), language_(language)
+{}
+
 void MdhdBox::print_structure(const unsigned int indent)
 {
   print_size_type(indent);
@@ -37,8 +50,35 @@ void MdhdBox::parse_data(MP4File & mp4, const uint64_t data_size)
     duration_ = mp4.read_uint32();
   }
 
-  uint32_t temp = mp4.read_uint32();
-  language_ = (temp >> 16) & 0x7FFF;
+  uint16_t data = mp4.read_uint16();
+  language_ = data & 0x7FFF;
+
+  mp4.read(2); /* pre-defined */
 
   check_data_left(mp4, data_size, init_offset);
+}
+
+void MdhdBox::write_box(MP4File & mp4)
+{
+  uint64_t size_offset = mp4.curr_offset();
+
+  write_size_type(mp4);
+  write_version_flags(mp4);
+
+  if (version() == 1) {
+    mp4.write_uint64(creation_time_);
+    mp4.write_uint64(modification_time_);
+    mp4.write_uint32(timescale_);
+    mp4.write_uint64(duration_);
+  } else {
+    mp4.write_uint32(creation_time_);
+    mp4.write_uint32(modification_time_);
+    mp4.write_uint32(timescale_);
+    mp4.write_uint32(duration_);
+  }
+
+  mp4.write_uint16(language_ & 0x7FFF);
+  mp4.write_zeros(2);
+
+  fix_size_at(mp4, size_offset);
 }
--- src/mp4/mdhd_box.hh
+++ src/mp4/mdhd_box.hh
@@ -9,17 +9,25 @@ class MdhdBox : public FullBox
 {
 public:
   MdhdBox(const uint64_t size, const std::string & type);
+  MdhdBox(const std::string & type,
+          const uint8_t version,
+          const uint32_t flags,
+          const uint64_t creation_time,
+          const uint64_t modification_time,
+          const uint32_t timescale,
+          const uint64_t duration,
+          const uint16_t language);
 
   /* accessors */
   uint64_t creation_time() { return creation_time_; }
   uint64_t modificaiton_time() { return modification_time_; }
   uint32_t timescale() { return timescale_; }
   uint64_t duration() { return duration_; }
-  uint16_t language() { return language_; }
 
   void print_structure(const unsigned int indent = 0);
 
   void parse_data(MP4File & mp4, const uint64_t data_size);
+  void write_box(MP4File & mp4);
 
 private:
   uint64_t creation_time_;
