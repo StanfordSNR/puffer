935249bf830037783df9599b4c8bd4dc38f91978
Keyi Zhang <keyi@stanford.edu>
2017-10-31 21:54:18 -0700

    Add mp4a parsing. esds spec (ISO 14496-1) is not free in public domain.

--- src/mp4/stsd_box.cc
+++ src/mp4/stsd_box.cc
@@ -25,6 +25,8 @@ void StsdBox::parse_data(MP4File & mp4, const uint64_t data_size)
     shared_ptr<Box> box;
     if (sample_type == "avc1") {
       box = make_shared<AVC1>(sample_size, sample_type);
+    } else if( sample_type == "mp4a") {
+      box = make_shared<AudioSampleEntry>(sample_size, sample_type);
     } else {
       box = make_shared<Box>(sample_size, sample_type);
     }
@@ -78,6 +80,50 @@ void VisualSampleEntry::parse_data(MP4File & mp4)
   }
 }
 
+AudioSampleEntry::AudioSampleEntry(const uint64_t size, const string & type)
+  : SampleEntry(size, type), sample_rate_()
+{}
+
+void AudioSampleEntry::parse_data(MP4File & mp4, const uint64_t data_size)
+{
+  uint64_t ini_pos = mp4.curr_offset();
+
+  SampleEntry::parse_data(mp4);
+
+  /* read reserved bytes 2 * 32 / 8 = 8 bytes */
+  mp4.read(8);
+  channel_count_ = mp4.read_uint16();
+  sample_size_ = mp4.read_uint16();
+  pre_defined_ = mp4.read_uint16();
+  mp4.read_uint16(); // reserved
+  sample_rate_ = mp4.read_uint32() >> 16;
+
+  /* Assume there is no other boxes in between */
+  uint32_t size = mp4.read_uint32();
+  string type = mp4.read(4);
+  if (type != "esds") {
+    cout << mp4.curr_offset() << endl;
+    throw runtime_error("Expecting esds inside mp4a box.");
+  }
+  auto box = make_shared<EsdsBox>(size, type);
+  box->parse_data(mp4, size - 8);
+  add_child(move(box));
+
+  check_data_left(mp4, data_size, ini_pos);
+}
+
+void AudioSampleEntry::print_structure(const unsigned int indent)
+{
+  print_size_type(indent);
+
+  string indent_str = string(indent + 2, ' ') + "| ";
+  cout << indent_str << "channel count " << channel_count_ << endl;
+  cout << indent_str << "sample size " << sample_size_ << endl;
+  cout << indent_str << "sample rate " << sample_rate_ << endl;
+
+  Box::print_structure(indent + 2);
+}
+
 AVC1::AVC1(const uint64_t size, const string & type)
   : VisualSampleEntry(size, type), avc_profile_(),
     avc_profile_compatibility_(), avc_level_(), avcc_size_()
@@ -119,4 +165,60 @@ void AVC1::parse_data(MP4File & mp4, const uint64_t data_size)
   avc_level_ = mp4.read_uint8();
 
   skip_data_left(mp4, data_size, init_offset);
+}
+
+
+EsdsBox::EsdsBox(const uint64_t size, const string & type)
+  : FullBox(size, type), es_id_(), stream_priority_(), object_type_(),
+    max_bitrate_(), avg_bitrate_()
+{}
+
+void EsdsBox::parse_data(MP4File & mp4, const uint64_t data_size)
+{
+  uint64_t init_pos = mp4.curr_offset();
+
+  parse_version_flags(mp4);
+  if (mp4.read_uint8() != type_tag) {
+    throw runtime_error("Expect esds to have a type tag");
+  }
+  read_tag_string(mp4);
+  /* size again */
+  mp4.read_uint8();
+  es_id_ = mp4.read_uint16();
+  stream_priority_ = mp4.read_uint8();
+  if (mp4.read_uint8() != config_type_tag) {
+    throw runtime_error("Expect config type tag");
+  }
+
+  read_tag_string(mp4);
+  /* size again */
+  mp4.read_uint8();
+  object_type_ = mp4.read_uint8();
+  /* ignore */
+  mp4.read_uint8();
+  /* ignore buffer size */
+  mp4.read(4);
+  max_bitrate_ = mp4.read_uint32();
+  avg_bitrate_ = mp4.read_uint32();
+
+  skip_data_left(mp4, data_size, init_pos);
+}
+
+void EsdsBox::read_tag_string(MP4File & mp4)
+{
+  for (int i = 0; i < 3; i++) {
+    uint8_t tag = mp4.read_uint8();
+    if (tag != tag_string_start and tag != tag_string_end) {
+      throw runtime_error("Expect 3 start/end tags");
+    }
+  }
+}
+void EsdsBox::print_structure(const unsigned int indent)
+{
+  print_size_type(indent);
+
+  string indent_str = string(indent + 2, ' ') + "| ";
+  cout << indent_str << "object type " << hex << unsigned(object_type_) << endl;
+  cout << indent_str << "max bitrate " << max_bitrate_ << endl;
+  cout << indent_str << "avg bitrate " << avg_bitrate_ << endl;
 }
