cff82efadffdb889827a2ca3633707e4f316327e
Francis Y. Yan <fyy@cs.stanford.edu>
2017-10-31 23:53:36 -0700

    Add write_box to tkhd box

--- src/mp4/tkhd_box.cc
+++ src/mp4/tkhd_box.cc
@@ -7,7 +7,26 @@ using namespace MP4;
 
 TkhdBox::TkhdBox(const uint64_t size, const string & type)
   : FullBox(size, type), creation_time_(), modification_time_(),
-    track_id_(), duration_(), width_(), height_()
+    track_id_(), duration_(), volume_(), width_(), height_()
+{}
+
+TkhdBox::TkhdBox(const string & type,
+                 const uint8_t version,
+                 const uint32_t flags,
+                 const uint64_t creation_time,
+                 const uint64_t modification_time,
+                 const uint32_t track_id,
+                 const uint64_t duration,
+                 const int16_t volume,
+                 const uint32_t width,
+                 const uint32_t height,
+                 const int16_t layer,
+                 const int16_t alternate_group,
+                 const vector<int32_t> matrix)
+  : FullBox(type, version, flags), creation_time_(creation_time),
+    modification_time_(modification_time), track_id_(track_id),
+    duration_(duration), volume_(volume), width_(width), height_(height),
+    layer_(layer), alternate_group_(alternate_group), matrix_(matrix)
 {}
 
 void TkhdBox::print_structure(const unsigned int indent)
@@ -45,15 +64,58 @@ void TkhdBox::parse_data(MP4File & mp4, const uint64_t data_size)
   }
 
   mp4.read(8); /* reserved */
-  mp4.read(2); /* layer */
-  mp4.read(2); /* alternate_group */
-  mp4.read(2); /* volume */
+  layer_ = mp4.read_int16();
+  alternate_group_ = mp4.read_int16();
+  volume_ = mp4.read_int16();
   mp4.read(2); /* reserved */
-  mp4.read(36);/* matrix */
+
+  vector<int32_t> matrix;
+  for (int i = 0; i < 9; ++i) {
+    matrix.emplace_back(mp4.read_int32());
+  }
+  matrix_ = move(matrix);
 
   /* width and height are 16.16 fixed-point numbers */
   width_ = mp4.read_uint32() / 65536;
   height_ = mp4.read_uint32() / 65536;
 
   check_data_left(mp4, data_size, init_offset);
+}
+
+void TkhdBox::write_box(MP4File & mp4)
+{
+  uint64_t size_offset = mp4.curr_offset();
+
+  write_size_type(mp4);
+  write_version_flags(mp4);
+
+  if (version() == 1) {
+    mp4.write_uint64(creation_time_);
+    mp4.write_uint64(modification_time_);
+    mp4.write_uint32(track_id_ );
+    mp4.write_zeros(4); /* reserved */
+    mp4.write_uint64(duration_);
+  } else {
+    mp4.write_uint32(creation_time_);
+    mp4.write_uint32(modification_time_);
+    mp4.write_uint32(track_id_);
+    mp4.write_zeros(4); /* reserved */
+    mp4.write_uint32(duration_);
+  }
+
+  mp4.write_zeros(8); /* reserved */
+  mp4.write_int16(layer_);
+  mp4.write_int16(alternate_group_);
+  mp4.write_int16(volume_);
+  mp4.write_zeros(2); /* reserved */
+
+  for (const auto & element : matrix_) {
+    mp4.write_int32(element);
+  }
+
+  /* width and height are 16.16 fixed-point numbers */
+  mp4.write_uint32(width_ * 65536);
+  mp4.write_uint32(height_ * 65536);
+
+  fix_size_at(mp4, size_offset);
 }
--- src/mp4/tkhd_box.hh
+++ src/mp4/tkhd_box.hh
@@ -9,6 +9,21 @@ class TkhdBox : public FullBox
 {
 public:
   TkhdBox(const uint64_t size, const std::string & type);
+  TkhdBox(const std::string & type,
+          const uint8_t version,
+          const uint32_t flags,
+          const uint64_t creation_time,
+          const uint64_t modification_time,
+          const uint32_t track_id,
+          const uint64_t duration,
+          const int16_t volume,
+          const uint32_t width,
+          const uint32_t height,
+          const int16_t layer = 0,
+          const int16_t alternate_group = 0,
+          const std::vector<int32_t> matrix = {
+            0x00010000, 0, 0, 0, 0x00010000, 0, 0, 0, 0x40000000
+          });
 
   /* accessors */
   uint64_t creation_time() { return creation_time_; }
@@ -21,14 +36,21 @@ public:
   void print_structure(const unsigned int indent = 0);
 
   void parse_data(MP4File & mp4, const uint64_t data_size);
+  void write_box(MP4File & mp4);
 
 private:
   uint64_t creation_time_;
   uint64_t modification_time_;
   uint32_t track_id_;
   uint64_t duration_;
+  int16_t volume_;
   uint32_t width_;
   uint32_t height_;
+  int16_t layer_ = 0;
+  int16_t alternate_group_ = 0;
+  std::vector<int32_t> matrix_ = {
+    0x00010000, 0, 0, 0, 0x00010000, 0, 0, 0, 0x40000000
+  };
 };
 
 }
