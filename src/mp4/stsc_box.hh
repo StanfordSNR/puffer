4c83947a519a37c36737999e518834ccc60e6881
Francis Y. Yan <fyy@cs.stanford.edu>
2017-11-05 00:01:49 -0700

    Parse stsc box

---
+++ src/mp4/stsc_box.hh
@@ -0,0 +1,37 @@
+#ifndef STSC_BOX_HH
+#define STSC_BOX_HH
+
+#include "box.hh"
+
+namespace MP4 {
+
+class StscBox : public FullBox
+{
+public:
+  struct Entry {
+    uint32_t first_chunk;
+    uint32_t samples_per_chunk;
+    uint32_t sample_description_index;
+  };
+
+  StscBox(const uint64_t size, const std::string & type);
+
+  /* accessors */
+  uint32_t entry_count() { return entries_.size(); }
+  std::vector<Entry> entries() { return entries_; }
+
+  /* mutators */
+  void set_entries(std::vector<Entry> entries);
+
+  void print_box(const unsigned int indent = 0);
+
+  void parse_data(MP4File & mp4, const uint64_t data_size);
+  void write_box(MP4File & mp4);
+
+private:
+  std::vector<Entry> entries_;
+};
+
+}
+
+#endif /* STSC_BOX_HH */
