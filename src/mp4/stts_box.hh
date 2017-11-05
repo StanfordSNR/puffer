71cb2255d40c28171cbc97feaa747cd6a984cba5
Francis Y. Yan <fyy@cs.stanford.edu>
2017-11-05 00:02:25 -0700

    Parse stts box

---
+++ src/mp4/stts_box.hh
@@ -0,0 +1,36 @@
+#ifndef STTS_BOX_HH
+#define STTS_BOX_HH
+
+#include "box.hh"
+
+namespace MP4 {
+
+class SttsBox : public FullBox
+{
+public:
+  struct Entry {
+    uint32_t sample_count;
+    uint32_t sample_delta;
+  };
+
+  SttsBox(const uint64_t size, const std::string & type);
+
+  /* accessors */
+  uint32_t entry_count() { return entries_.size(); }
+  std::vector<Entry> entries() { return entries_; }
+
+  /* mutators */
+  void set_entries(std::vector<Entry> entries);
+
+  void print_box(const unsigned int indent = 0);
+
+  void parse_data(MP4File & mp4, const uint64_t data_size);
+  void write_box(MP4File & mp4);
+
+private:
+  std::vector<Entry> entries_;
+};
+
+}
+
+#endif /* STTS_BOX_HH */
