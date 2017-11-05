6a0a24afbebd9178f0a1847c8a6a151728a4d42e
Keyi Zhang <keyi@stanford.edu>
2017-11-05 13:35:45 -0800

    Remove loop in forwarder.test. Use function timeout instead

--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -4,7 +4,8 @@ import errno
 import socket
 import subprocess
 from subprocess import PIPE
-
+import signal
+from functools import wraps
 
 def get_open_port():
     sock = socket.socket(socket.AF_INET)
@@ -54,3 +55,25 @@ def check_output(cmd, **kwargs):
 def Popen(cmd, **kwargs):
     print_cmd(cmd)
     return subprocess.Popen(cmd, **kwargs)
+
+class TimeoutError(Exception):
+    pass
+
+# from https://stackoverflow.com/a/2282656
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
