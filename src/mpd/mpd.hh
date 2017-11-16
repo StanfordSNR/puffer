44c1d347d63d141bbdd3e6b6efbed760aab2d72b
Keyi Zhang <keyi@stanford.edu>
2017-11-15 22:17:41 -0800

    Change mpd_writer back to dynamic and add UTCTiming

--- src/mpd/mpd.hh
+++ src/mpd/mpd.hh
@@ -76,12 +76,11 @@ struct Representation {
   MimeType type;
   uint32_t timescale;
   uint32_t duration;
-  uint32_t start_number;
 
   Representation(std::string id, unsigned int bitrate, MimeType type,
-                 uint32_t timescale, uint32_t duration, uint32_t start_number)
+                 uint32_t timescale, uint32_t duration)
     : id(id), bitrate(bitrate), type(type), timescale(timescale),
-      duration(duration), start_number(start_number)
+      duration(duration)
   {}
 
   virtual ~Representation() {}
@@ -97,10 +96,8 @@ struct VideoRepresentation : public Representation {
   VideoRepresentation(
       std::string id, unsigned int width, unsigned int height,
       unsigned int bitrate, uint8_t profile, unsigned int avc_level,
-      float framerate, uint32_t timescale, uint32_t duration,
-      uint32_t start_number)
-    : Representation(id, bitrate, MimeType::Video,
-                     timescale, duration, start_number),
+      float framerate, uint32_t timescale, uint32_t duration)
+    : Representation(id, bitrate, MimeType::Video, timescale, duration),
       width(width), height(height), profile(profile),
       avc_level(avc_level), framerate(framerate)
   {
@@ -115,9 +112,8 @@ struct AudioRepresentation : public Representation {
 
   AudioRepresentation(
       std::string id, unsigned int bitrate, unsigned int sampling_rate,
-      MimeType mp4a, uint32_t timescale, uint32_t duration,
-      uint32_t start_number)
-    : Representation(id, bitrate, mp4a, timescale, duration, start_number),
+      MimeType mp4a, uint32_t timescale, uint32_t duration)
+    : Representation(id, bitrate, mp4a, timescale, duration),
       sampling_rate(sampling_rate)
   {
     /* check mp4a here */
@@ -142,7 +138,6 @@ public:
   uint32_t timescale() { return timescale_; }
   virtual std::set<std::shared_ptr<Representation>> get_repr()
   { return std::set<std::shared_ptr<Representation>>(); }
-  uint32_t start_number() { return start_number_; }
   uint32_t presentation_time_offset() { return presentation_time_offset_; }
 
   void set_presentation_time_offset(uint32_t value)
@@ -164,7 +159,6 @@ private:
   std::string init_uri_;
   std::string media_uri_;
   uint32_t duration_;
-  uint32_t start_number_ = 0;
   uint32_t presentation_time_offset_ = 0;
   uint32_t timescale_ = 0;
 };
@@ -217,27 +211,21 @@ inline bool operator<(const AdaptionSet & a, const AdaptionSet & b)
 class MPDWriter
 {
 public:
-  MPDWriter(uint32_t medua_duration, uint32_t min_buffer_time,
-            std::string base_url);
+  MPDWriter(uint32_t min_buffer_time, std::string base_url,
+            std::string time_url);
   ~MPDWriter();
   std::string flush();
   void add_video_adaption_set(std::shared_ptr<MPD::VideoAdaptionSet> set);
   void add_audio_adaption_set(std::shared_ptr<MPD::AudioAdaptionSet> set);
   void set_publish_time(const std::chrono::seconds time)
   { publish_time_ = time; }
-  void set_video_start_number(uint32_t number)
-  { v_start_number_ = number; }
-  void set_audio_start_number(uint32_t number)
-  { a_start_number_ = number; }
 
 private:
-  uint32_t media_duration_;
   uint32_t min_buffer_time_;
   std::chrono::seconds publish_time_;
   std::unique_ptr<XMLWriter> writer_;
   std::string base_url_;
-  uint32_t v_start_number_ = 0;
-  uint32_t a_start_number_ = 0;
+  std::string time_url_;
 
   std::set<std::shared_ptr<MPD::VideoAdaptionSet>> video_adaption_set_;
   std::set<std::shared_ptr<MPD::AudioAdaptionSet>> audio_adaption_set_;
@@ -254,6 +242,9 @@ private:
   void write_audio_repr(std::shared_ptr<MPD::AudioRepresentation> repr);
   std::string convert_pt(unsigned int seconds);
   void write_framerate(const float & framerate);
+
+  const std::string utctiming_scheme = "urn:mpeg:dash:utc:http-xsdate:2014";
+  const std::string mpd_type = "dynamic";
 };
 
 #endif /* TV_ENCODER_MPD_HH */
