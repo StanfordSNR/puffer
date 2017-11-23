cceb5bbb3ad416d8ec9369a456fa99c28db1ff1f
Keyi Zhang <keyi@stanford.edu>
2017-11-23 00:07:38 -0800

    Add crf option in video-encoder.sh

--- src/wrappers/video-encoder.sh
+++ src/wrappers/video-encoder.sh
@@ -6,8 +6,9 @@ src_fname=$(basename $src_path)
 src_fname_prefix=${src_fname%.*}
 dst_dir=$2
 res=$3
+crf=$4
 tmp_file=`mktemp /tmp/tmp.XXXXXX.mp4`
 
-ffmpeg -y -i $src_path -c:v libx264 -s $res -crf 23 -preset veryfast \
+ffmpeg -y -i $src_path -c:v libx264 -s $res -crf $crf -preset veryfast \
   -video_track_timescale 90000 $tmp_file
 mv $tmp_file $dst_dir/$src_fname_prefix.mp4
