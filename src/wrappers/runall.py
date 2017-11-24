e4399680659a2aade5434298fb89e5e05f70c408
Keyi Zhang <keyi@stanford.edu>
2017-11-23 19:11:04 -0800

    Refactor runall.py. Needs further refactoring.

--- src/wrappers/runall.py
+++ src/wrappers/runall.py
@@ -6,25 +6,24 @@ import argparse
 import subprocess
 from collections import namedtuple
 import logging
-import time
 
 # current python file location
-FILE_DIR            = os.path.dirname(os.path.realpath(__file__))
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 # use a mock decoder for now
-DECODER_PATH        = os.path.join(FILE_DIR, "..", "mock", "decoder")
-CANONICALIZER_PATH  = os.path.join(FILE_DIR, "canonicalizer.sh")
-VIDEO_ENCODER_PATH  = os.path.join(FILE_DIR, "video-encoder.sh")
-AUDIO_ENCODER_PATH  = os.path.join(FILE_DIR, "audio-encoder.sh")
+DECODER_PATH = os.path.join(FILE_DIR, "..", "mock", "decoder")
+CANONICALIZER_PATH = os.path.join(FILE_DIR, "canonicalizer.sh")
+VIDEO_ENCODER_PATH = os.path.join(FILE_DIR, "video-encoder.sh")
+AUDIO_ENCODER_PATH = os.path.join(FILE_DIR, "audio-encoder.sh")
 VIDEO_FRAGMENT_PATH = os.path.join(FILE_DIR, "video-fragment.sh")
 AUDIO_FRAGMENT_PATH = os.path.join(FILE_DIR, "audio-fragment.sh")
-TIME_PATH           = os.path.join(FILE_DIR, "..", "time", "time")
-MPD_WRITER_PATH     = os.path.join(FILE_DIR, "..", "mpd", "mpd_writer")
-NOTIFIER_PATH       = os.path.join(FILE_DIR, "..", "notifier", "run_notifier")
-MONITOR_PATH        = os.path.join(FILE_DIR, "..", "notifier", "monitor")
+TIME_PATH = os.path.join(FILE_DIR, "..", "time", "time")
+MPD_WRITER_PATH = os.path.join(FILE_DIR, "..", "mpd", "mpd_writer")
+NOTIFIER_PATH = os.path.join(FILE_DIR, "..", "notifier", "run_notifier")
+MONITOR_PATH = os.path.join(FILE_DIR, "..", "notifier", "monitor")
 
 # filename constants
-AUDIO_INIT_NAME     = "init.webm"
-VIDEO_INIT_NAME     = "init.mp4"
+AUDIO_INIT_NAME = "init.webm"
+VIDEO_INIT_NAME = "init.mp4"
 
 # wrapper tuple
 VideoConfig = namedtuple("VideoConfig", ["width", "height", "crf"])
@@ -38,18 +37,18 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 def configure_args():
-    parser= argparse.ArgumentParser("Run all pipeline components")
-    parser.add_argument("-vf", "--video-format", action = "append", nargs = '+',
-            metavar = ("res","crf"), help = "specify the output video format",
+    parser = argparse.ArgumentParser("Run all pipeline components")
+    parser.add_argument("-vf", "--video-format", action="append", nargs='+',
+            metavar=("res","crf"), help="specify the output video format",
             dest = "video_format", required = True)
-    parser.add_argument("-af", "--audio-format", action = "store", nargs = '+',
-            metavar = ("bitrate"), help = "specify the output audio format",
-            dest = "audio_format", required = True, type=int)
-    parser.add_argument("-p", "--port", action = "store", required = True,
-            help = "TCP port number", type = int, dest = "port")
-    parser.add_argument("-o", "--output", action = "store", required = True,
-            help = "output folder. It will be used as BaseURL as well.",
-            dest = "output")
+    parser.add_argument("-af", "--audio-format", action="store", nargs='+',
+            metavar=("bitrate"), help="specify the output audio format",
+            dest="audio_format", required=True, type=int)
+    parser.add_argument("-p", "--port", action="store", required=True,
+            help = "TCP port number", type=int, dest="port")
+    parser.add_argument("-o", "--output", action="store", required=True,
+            help="output folder. It will be used as BaseURL as well.",
+            dest="output")
     parser.add_argument("-m", "--mock-file", action="store",
             help="mock video file", dest="mock_file")
     return parser
@@ -64,20 +63,26 @@ def check_res(res):
     return width, height
 
 def check_dir(*args):
-    for d in args:
-        if not os.path.isdir(d):
-            os.makedirs(d)
+    for dirname in args:
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
 
 def run_process(command_str):
     # this will ensure the subprocess will continue to run even the bootstrap
     # script exits as well as prevent zombie process
     splitted_commands = command_str.split()
-    p = subprocess.Popen(splitted_commands,
+    process = subprocess.Popen(splitted_commands,
                      preexec_fn=os.setpgrp)
 
     name = os.path.basename(splitted_commands[0])
-    logger.info("Start a new process [" + str(p.pid) + "]: " + name + \
+    logger.info("Start a new process [" + str(process.pid) + "]: " + name + \
             ". commands: " + " ".join(splitted_commands[1:]))
+
+def run_notifier(notifier_command):
+    run_process(NOTIFIER_PATH + " " + notifier_command)
+
+def run_monitor(monitor_command):
+    run_process(MONITOR_PATH + " " + monitor_command)
 
 def get_video_output(output_dir, fmt):
     return os.path.join(output_dir, fmt.width + "x" + fmt.height + "-" + \
@@ -85,6 +90,9 @@ def get_video_output(output_dir, fmt):
 
 def get_audio_output(output_dir, bitrate):
     return os.path.join(output_dir, bitrate)
+
+def combine_args(*args):
+    return " ".join(args)
 
 def main():
     parser = configure_args()
@@ -116,16 +124,16 @@ def main():
     video_raw_output = os.path.join(output_folder, "video_raw")
     check_dir(audio_raw_output, video_raw_output)
     # this is only for mock interface
-    decoder_command = "-p " + str(port_number) + " -a " + audio_raw_output +\
-            " -v " + video_raw_output + " -m " + mock_file
+    decoder_command = combine_args("-p", str(port_number), "-a", audio_raw_output,
+            "-v", video_raw_output, "-m", mock_file)
     run_process(DECODER_PATH + " " + decoder_command)
 
     # video canonicalizer
     video_canonical = os.path.join(output_folder, "video_canonical")
     check_dir(video_canonical)
-    notifier_command = video_raw_output + " " + video_canonical + " " + \
-            CANONICALIZER_PATH
-    run_process(NOTIFIER_PATH + " " + notifier_command)
+    notifier_command = combine_args(video_raw_output, video_canonical,
+            CANONICALIZER_PATH)
+    run_notifier(notifier_command)
 
     # (notifier + video_encoder) + (notifier + video_frag)
     for fmt in video_formats:
@@ -137,19 +145,19 @@ def main():
         output_list.append(final_output)
         # mkdir
         check_dir(final_output, encoded_output)
-        notifier_command = video_canonical + " " + encoded_output + " " + \
-                VIDEO_ENCODER_PATH + " " + res  + " " + fmt.crf
+        notifier_command = combine_args(video_canonical, encoded_output,
+                VIDEO_ENCODER_PATH, res, fmt.crf)
         # run notifier to encode
-        run_process(NOTIFIER_PATH + " " + notifier_command)
+        run_notifier(notifier_command)
 
         # run frag to output the final segment
-        notifier_command = encoded_output + " " + final_output + " " + \
-                VIDEO_FRAGMENT_PATH
-        run_process(NOTIFIER_PATH + " " + notifier_command)
+        notifier_command = combine_args(encoded_output, final_output,
+                VIDEO_FRAGMENT_PATH)
+        run_notifier(notifier_command)
         # init segment. use monitor's run once command
-        monitor_command = "-q " + encoded_output + " -exec " + \
-                VIDEO_FRAGMENT_PATH + " {} -i " + VIDEO_INIT_NAME
-        run_process(MONITOR_PATH + " " + monitor_command)
+        monitor_command = combine_args("-q", encoded_output, "-exec",
+                VIDEO_FRAGMENT_PATH, "{}", "-i", VIDEO_INIT_NAME)
+        run_monitor(monitor_command)
 
 
     # (notifier + video_encoder) + (notifier + video_frag)
@@ -160,31 +168,30 @@ def main():
         output_list.append(final_output)
         # mkdir
         check_dir(final_output, encoded_output)
-        notifier_command = audio_raw_output + " " + encoded_output + " " + \
-                AUDIO_ENCODER_PATH + " " + bitrate
-        logger.info("Using notifier command " + notifier_command)
+        notifier_command = combine_args(audio_raw_output, encoded_output,
+                AUDIO_ENCODER_PATH, bitrate)
         # run notifier to encode
-        run_process(NOTIFIER_PATH + " " + notifier_command)
+        run_notifier(notifier_command)
 
         # run frag to output the final segment
         # media segment
-        notifier_command = encoded_output + " " + final_output + " " + \
-                AUDIO_FRAGMENT_PATH
-        run_process(NOTIFIER_PATH + " " + notifier_command)
+        notifier_command = combine_args(encoded_output, final_output,
+                AUDIO_FRAGMENT_PATH)
+        run_notifier(notifier_command)
         # init segment. use monitor's run once command
-        monitor_command = "-q " + encoded_output + " -exec " + \
-                AUDIO_FRAGMENT_PATH + " {} -i " + AUDIO_INIT_NAME
-        run_process(MONITOR_PATH + " " + monitor_command)
+        monitor_command = combine_args("-q", encoded_output, "-exec",
+                AUDIO_FRAGMENT_PATH, "{}", "-i", AUDIO_INIT_NAME)
+        run_monitor(monitor_command)
 
 
     # run time/time
     time_output = os.path.join(output_folder, "time")
     time_dirs = " ".join([get_video_output(output_folder, fmt) for fmt in \
             video_formats])
-    monitor_command = "-a " + time_dirs + " " + output_folder + " -exec " + \
-            TIME_PATH + " -i {} -o " + time_output
+    monitor_command = combine_args("-a", time_dirs, output_folder, "-exec",
+            TIME_PATH, "-i", "{}", "-o", time_output)
     # run monitor to update the time
-    run_process(MONITOR_PATH + " " + monitor_command)
+    run_monitor(monitor_command)
 
     # run monitor + mpd_writer for all the output directories
     audio_dir = " ".join([get_audio_output(output_folder, bitrate) for bitrate \
@@ -192,10 +199,9 @@ def main():
     media_dir = time_dirs[:] + " " + audio_dir
     mpd_path = os.path.join(output_folder, "live.mpd")
     # it will try its best to produce the mpd
-    monitor_command = "-a -q " + media_dir + " -exec " + MPD_WRITER_PATH + \
-            " -u " + output_folder + " -t time -o " + mpd_path + \
-            " --ignore {}"
-    run_process(MONITOR_PATH + " " + monitor_command)
+    monitor_command = combine_args("-a", "-q", media_dir, "-exec", MPD_WRITER_PATH,
+            "-u", output_folder, "-t", "time", "-o", mpd_path, "--ignore", "{}")
+    run_monitor(monitor_command)
 
 if __name__ == "__main__":
     main()
