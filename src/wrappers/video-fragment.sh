ab86cef4f99d9c87fcfb795942174f9028f912c0
Francis Y. Yan <fyy@cs.stanford.edu>
2017-12-01 17:15:24 -0800

    Fix file extensions in audio/video-fragment.sh

--- src/wrappers/video-fragment.sh
+++ src/wrappers/video-fragment.sh
@@ -5,7 +5,7 @@ src_path=$1
 src_fname=$(basename $src_path)
 src_fname_prefix=${src_fname%.*}
 dst_dir=$2
-tmp_file=`mktemp /tmp/tmp.XXXXXX.webm`
+tmp_file=`mktemp /tmp/tmp.XXXXXX.m4s`
 mp4_frag=$curr_dir/../mp4/mp4_fragment
 
 $mp4_frag -m $tmp_file $src_path
