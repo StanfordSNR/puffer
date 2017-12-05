9fb01c7323544de6ccb261a294849d3df4e99a4f
Keyi Zhang <keyi@stanford.edu>
2017-12-05 11:33:33 -0800

    No need to have notifier destructor since it's handled by FileDescriptor

--- src/notifier/notifier.hh
+++ src/notifier/notifier.hh
@@ -19,7 +19,6 @@ public:
                                         const std::string &)>;
 
   Notifier();
-  ~Notifier();
 
   /* add one or more paths to the watch list */
   int add_watch(const std::string & path,
