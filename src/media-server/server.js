2ff3ad49866800d322bc26a45cb8f691f7e815f8
Francis Y. Yan <fyy@cs.stanford.edu>
2018-01-14 17:34:25 -0800

    Add a simple server.js that receives websocket messages

---
+++ src/media-server/server.js
@@ -0,0 +1,26 @@
+if (process.argv.length != 3) {
+  console.log('Usage: node index.js <port-number>');
+  process.exit(-1);
+}
+
+const port_num = Number(process.argv[2]);
+
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+wss.on('connection', function(ws, req) {
+  ws.on('message', function(message) {
+    console.log('Received: %s', message);
+  });
+
+  ws.send('Hello from server');
+});
+
+server.listen(port_num, function() {
+  console.log('Listening on %d', server.address().port);
+});
