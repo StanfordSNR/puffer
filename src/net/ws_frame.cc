63ba626b7a58469c9e6c6e1b0f59f3ff17411fb0
Sadjad Fouladi <sfouladi@gmail.com>
2018-01-17 16:41:59 -0800

    ws_frame: Create serialization function.

--- src/net/ws_frame.cc
+++ src/net/ws_frame.cc
@@ -2,8 +2,67 @@
 
 #include "ws_frame.hh"
 
-void WebSocketFrame::set_payload(const std::string & payload)
+#include <endian.h>
+
+using namespace std;
+
+string put_field(const uint16_t n)
 {
-  payload_ = payload;
-  payload_length_ = payload.length();
+  const uint16_t network_order = htole16(n);
+  return string(reinterpret_cast<const char *>(&network_order),
+                sizeof(network_order));
+}
+
+string put_field(const uint32_t n)
+{
+  const uint32_t network_order = htole32(n);
+  return string(reinterpret_cast<const char *>(&network_order),
+                sizeof(network_order));
+}
+
+string put_field(const uint64_t n)
+{
+  const uint32_t network_order = htole64(n);
+  return string(reinterpret_cast<const char *>(&network_order),
+                sizeof(network_order));
+}
+
+
+string WebSocketFrame::to_string() const
+{
+  string output;
+  uint8_t temp_byte;
+
+  /* first byte */
+  temp_byte = (fin_ << 8) + static_cast<uint8_t>(opcode_);
+  output.append(1, temp_byte);
+
+  /* second byte */
+  temp_byte = (masking_key_.initialized() << 8);
+
+  if (payload_.length() <= 125u) {
+    temp_byte += static_cast<uint8_t>(payload_.length());
+    output.append(1, temp_byte);
+  }
+  else if (payload_.length() < (1u << 16)) {
+    temp_byte += static_cast<uint8_t>(126);
+    output.append(1, temp_byte);
+    output += put_field(static_cast<uint16_t>(payload_.length()));
+  }
+  else if (payload_.length() <= (1ull << 63) ){
+    temp_byte += static_cast<uint8_t>(127);
+    output.append(1, temp_byte);
+    output += put_field(static_cast<uint64_t>(payload_.length()));
+  }
+  else {
+    throw runtime_error("payload size > maximum allowed");
+  }
+
+  if ( masking_key_.initialized() ) {
+    output += put_field(*masking_key_);
+  }
+
+  output += payload_;
+
+  return output;
 }
--- src/net/ws_frame.hh
+++ src/net/ws_frame.hh
@@ -9,7 +9,7 @@
 class WebSocketFrame
 {
 public:
-  enum class OpCode
+  enum class OpCode : uint8_t
   {
     Continuation = 0x0, Text = 0x1, Binary = 0x2, Close = 0x8,
     Ping = 0x9, Pong = 0xA
@@ -18,22 +18,23 @@ public:
 private:
   bool fin_ {false};
   OpCode opcode_ {OpCode::Binary};
-  uint64_t payload_length_ {0};
   Optional<uint32_t> masking_key_ {false};
   std::string payload_ {};
 
 public:
-
   void set_fin(const bool fin) { fin_ = fin; }
   void set_opcode(const OpCode opcode) { opcode_ = opcode; }
   void set_masking(const uint32_t masking_key) { masking_key_.reset(masking_key); }
-  void set_payload(const std::string & payload);
+  void set_payload(const std::string & payload) { payload_ = payload; }
 
   bool fin() const { return fin_; }
   OpCode opcode() const { return opcode_; }
-  uint64_t payload_length() const { return payload_length_; }
+  uint64_t payload_length() const { return payload_.length(); }
   bool masked() const { return masking_key_.initialized(); }
   const std::string & payload() const { return payload_; }
+
+  /* serialize a frame */
+  std::string to_string() const;
 };
 
 #endif /* WS_FRAME_HH */
