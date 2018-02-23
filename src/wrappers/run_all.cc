70c4f915fa04c40a75e4dffc2231fe4a3a6c61cd
Francis Y. Yan <fyy@cs.stanford.edu>
2018-02-22 23:33:21 -0800

    Add config.yml and test parsing

--- src/wrappers/run_all.cc
+++ src/wrappers/run_all.cc
@@ -1,8 +1,24 @@
+#include <iostream>
+#include <string>
 #include "yaml-cpp/yaml.h"
 
 using namespace std;
 
 int main()
 {
+  YAML::Node config = YAML::LoadFile("config.yml");
+
+  const YAML::Node & ress = config["test"]["video"];
+  for (YAML::const_iterator res = ress.begin(); res != ress.end(); ++res) {
+    cout << res->first.as<string>() << ": ";
+
+    const YAML::Node & crfs = res->second;
+    for (YAML::const_iterator crf = crfs.begin(); crf != crfs.end(); ++crf) {
+      cout << crf->as<int>() << " ";
+    }
+
+    cout << endl;
+  }
+
   return EXIT_SUCCESS;
 }
