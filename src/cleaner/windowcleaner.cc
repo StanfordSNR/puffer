fa44fd2d82a56be152c8bda7eaa166b857e21ce7
James Hong <james.hong@cs.stanford.edu>
2018-02-23 16:14:44 -0800

    Add test for cleaner

--- src/cleaner/windowcleaner.cc
+++ src/cleaner/windowcleaner.cc
@@ -16,7 +16,7 @@ void print_usage(const string & program_name)
   "Usage: " << program_name << " <input_file> <clean_ext> <time_window>\n\n"
   "<input_file>   input file from notifier\n"
   "<clean_ext>    extension of the files to clean\n"
-  "<time_window>  clean files with names < input_file - time_window"
+  "<time_window>  clean files with timestamped names that are less than input_file - time_window"
   << endl;
 }
 
@@ -32,21 +32,25 @@ int main(int argc, char * argv[])
     return EXIT_FAILURE;
   }
 
-  string input_file, clean_dir, clean_ext;
-  int64_t time_window;
+  string input_file, clean_ext;
   input_file = argv[1];
-  clean_dir = argv[2];
-  clean_ext = argv[3];
-  time_window = stoll(argv[4]);
+  clean_ext = argv[2];
+  int64_t time_window = stoll(argv[3]);
 
-  if (!fs::exists(clean_dir)) {
-    cerr << "Directory to clean does not exist " << clean_dir << endl;
+  if (time_window <= 0) {
+    cerr << "Time window cannot be negative or less than 0" << endl;
     return EXIT_FAILURE;
   }
 
   string input_file_basename = get_file_basename(input_file);
   string input_file_dir = input_file.substr(0,
                           input_file.length() - input_file_basename.length());
+
+  if (!fs::exists(input_file_dir)) {
+    cerr << "Input file directory does not exist " << input_file_dir << endl;
+    return EXIT_FAILURE;
+  }
+
   auto split_input_file_basename = split_filename(input_file_basename);
   if (split_input_file_basename.second != clean_ext) {
     cerr << input_file << " is not of type " << clean_ext << endl;
@@ -59,7 +63,7 @@ int main(int argc, char * argv[])
   for (const auto & entry : fs::directory_iterator(input_file_dir)) {
     string path = entry.path().string();
     string basename = get_file_basename(path);
-    auto split_file_basename = split_filename(input_file_basename);
+    auto split_file_basename = split_filename(basename);
 
     /* File is of the type to clean */
     if (split_file_basename.second == clean_ext) {
