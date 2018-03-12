322ef5f71f834807722d236adfa76d412918a392
Francis Y. Yan <fyy@cs.stanford.edu>
2018-03-12 14:20:50 -0700

    Test sending video chunks every 10 ms

--- src/media-server/ws_client.hh
+++ src/media-server/ws_client.hh
@@ -10,6 +10,16 @@ public:
                   const uint64_t next_vts,
                   const uint64_t next_ats);
 
+  /* accessors */
+  uint64_t next_vts() { return next_vts_; }
+  uint64_t next_ats() { return next_ats_; }
+
+  int playback_buf() { return playback_buf_; }
+
+  /* mutators */
+  void set_next_vts(const uint64_t next_vts) { next_vts_ = next_vts; }
+  void set_next_ats(const uint64_t next_ats) { next_ats_ = next_ats; }
+
 private:
   uint64_t connection_id_;
 
