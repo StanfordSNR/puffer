24144bcdc1bced96cadc9f3a3661b8d08c7e53b1
Francis Y. Yan <fyy@cs.stanford.edu>
2018-03-13 22:15:27 -0700

    ws_media_server.cc and ws_client.cc: use VideoFormat and AudioFormat

--- src/media-server/ws_client.hh
+++ src/media-server/ws_client.hh
@@ -5,24 +5,28 @@
 #include <optional>
 #include <tuple>
 
+#include "yaml.hh"
+
 class WebSocketClient
 {
 public:
   WebSocketClient(const uint64_t connection_id);
 
-  void initialize(const string & channel, const uint64_t vts, 
-                  const uint64_t ats);
+  void initialize(const std::string & channel,
+                  const uint64_t vts, const uint64_t ats);
 
   /* accessors */
   uint64_t connection_id() { return connection_id_; }
   std::optional<std::string> channel() { return channel_; }
+
   std::optional<uint64_t> next_vts() { return next_vts_; }
   std::optional<uint64_t> next_ats() { return next_ats_; }
+
   double video_playback_buf() { return video_playback_buf_; }
   double audio_playback_buf() { return audio_playback_buf_; }
 
-  std::optional<std::tuple<string, string>> curr_vq() { return curr_vq_; }
-  std::optional<std::string> curr_aq() { return curr_aq_; }
+  std::optional<VideoFormat> curr_vq() { return curr_vq_; }
+  std::optional<AudioFormat> curr_aq() { return curr_aq_; }
 
   /* mutators */
   void set_next_vts(const uint64_t next_vts) { next_vts_ = next_vts; }
@@ -31,8 +35,8 @@ public:
   void set_audio_playback_buf(const double buf) { audio_playback_buf_ = buf; }
   void set_video_playback_buf(const double buf) { video_playback_buf_ = buf; }
 
-  void set_curr_vq(const tuple<string, string> quality) { curr_vq_ = quality; }
-  void set_curr_aq(const string & quality) { curr_aq_ = quality; }
+  void set_curr_vq(const VideoFormat quality) { curr_vq_ = quality; }
+  void set_curr_aq(const AudioFormat & quality) { curr_aq_ = quality; }
 
 private:
   uint64_t connection_id_;
@@ -42,8 +46,8 @@ private:
   std::optional<uint64_t> next_vts_;
   std::optional<uint64_t> next_ats_;
 
-  std::optional<std::tuple<string, string>> curr_vq_;
-  std::optional<std::string> curr_aq_;
+  std::optional<VideoFormat> curr_vq_;
+  std::optional<AudioFormat> curr_aq_;
 
   double video_playback_buf_;
   double audio_playback_buf_;
