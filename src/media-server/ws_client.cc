15ede6e180ef096b39dad90c5c57da19cff5e6ad
Francis Y. Yan <fyy@cs.stanford.edu>
2018-03-13 22:25:11 -0700

    Finish cleaning up ws_client.hh

--- src/media-server/ws_client.cc
+++ src/media-server/ws_client.cc
@@ -1,5 +1,7 @@
 #include "ws_client.hh"
 
+using namespace std;
+
 WebSocketClient::WebSocketClient(const uint64_t connection_id)
   : connection_id_(connection_id),
     channel_(), next_vts_(), next_ats_(),
@@ -7,8 +9,8 @@ WebSocketClient::WebSocketClient(const uint64_t connection_id)
     video_playback_buf_(), audio_playback_buf_()
 {}
 
-WebSocketClient::initialize(const string & channel,
-                            const uint64_t vts, const uint64_t ats)
+void WebSocketClient::initialize(const string & channel,
+                                 const uint64_t vts, const uint64_t ats)
 {
   channel_ = channel;
 
