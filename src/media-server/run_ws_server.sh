4e79ab1c5a6c1315fe788e2c9aa6eef3c87ce2dd
James Hong <hongjn@cs.stanford.edu>
2018-03-15 11:31:28 -0700

    Minor fix in ws server script

--- src/media-server/run_ws_server.sh
+++ src/media-server/run_ws_server.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./run_ws_server.sh <yaml_config>"
+  exit
+fi
+
 config=$1
 
-cd static
-python3 -m http.server 8080 &
+./ws_media_server $config &
 child_pid=$!
+
+cd static
+python3 -m http.server 8080
 echo HTTP pid $child_pid
 cd ..
-
-./ws_media_server $config
 
 kill -9 $child_pid
