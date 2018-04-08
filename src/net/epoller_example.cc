c1c7b50f3752ebc8669e19e4fa4a34d6ddf7c028
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-08 14:07:26 -0700

    Adapt epoller_example.cc to the new interface of Epoller

--- src/net/epoller_example.cc
+++ src/net/epoller_example.cc
@@ -1,4 +1,6 @@
+#include <cassert>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <deque>
 #include <map>
@@ -10,7 +12,7 @@
 using namespace std;
 
 /* map client fd to socket instance */
-map<int, TCPSocket> clients;
+map<int, shared_ptr<TCPSocket>> clients;
 
 /* queue of keyboard input data */
 deque<string> pending_data;
@@ -20,61 +22,63 @@ int main()
   /* create Epoller */
   auto epoller = make_shared<Epoller>();
 
-  FileDescriptor keyboard(STDIN_FILENO);
-  epoller->add_events(keyboard, EPOLLIN);
-  epoller->set_callback(keyboard, EPOLLIN,
-    [&keyboard]() {
-      pending_data.emplace_back(keyboard.read());
+  auto keyboard = make_shared<FileDescriptor>(STDIN_FILENO);
+  epoller->register_fd(keyboard, EPOLLIN);
+  epoller->set_callback(keyboard->fd_num(), EPOLLIN,
+    [keyboard_weak = weak_ptr<FileDescriptor>(keyboard)]() {
+      auto keyboard = keyboard_weak.lock();
+      assert(keyboard);
+
+      pending_data.emplace_back(keyboard->read());
       return 0;
     }
   );
 
   /* create TCP listening socket */
-  TCPSocket listening_socket;
-  listening_socket.set_blocking(false);
-  listening_socket.set_reuseaddr();
-  listening_socket.bind(Address("0", 12345));
-  listening_socket.listen();
+  auto listening_socket = make_shared<TCPSocket>();
+  listening_socket->set_blocking(false);
+  listening_socket->set_reuseaddr();
+  listening_socket->bind(Address("0", 12345));
+  listening_socket->listen();
 
-  epoller->add_events(listening_socket, EPOLLIN);
-  epoller->set_callback(listening_socket, EPOLLIN,
-    [epoller_weak_ptr = weak_ptr<Epoller>(epoller), &listening_socket]() {
-      auto epoller = epoller_weak_ptr.lock();
-      if (not epoller) {
-        cerr << "Epoller is gone" << endl;
-        return -1;
-      }
+  epoller->register_fd(listening_socket, EPOLLIN);
+  epoller->set_callback(listening_socket->fd_num(), EPOLLIN,
+    [epoller_weak = weak_ptr<Epoller>(epoller),
+     listening_socket_weak = weak_ptr<TCPSocket>(listening_socket)]() {
+      auto epoller = epoller_weak.lock();
+      assert(epoller);
 
-      cerr << "EPOLLIN event from listening socket" << endl;
+      auto listening_socket = listening_socket_weak.lock();
+      assert(listening_socket);
 
-      TCPSocket client_orig = listening_socket.accept();
-      int client_fd = client_orig.fd_num();
-      clients.emplace(client_fd, move(client_orig));
+      cerr << "Epoller " << epoller->fd_num()
+           << ": EPOLLIN event from listening socket" << endl;
 
-      TCPSocket & client = clients.at(client_fd);
-      epoller->add_events(client, EPOLLIN);
-      epoller->set_callback(client, EPOLLIN,
-        [epoller_weak_ptr = weak_ptr<Epoller>(epoller), client_fd]() {
-          auto epoller = epoller_weak_ptr.lock();
-          if (not epoller) {
-            cerr << "Epoller is gone" << endl;
-            return -1;
-          }
+      auto client = make_shared<TCPSocket>(listening_socket->accept());
+      clients.emplace(client->fd_num(), client);
 
-          cerr << "EPOLLIN event from client " << client_fd << endl;
+      epoller->register_fd(client, EPOLLIN);
+      epoller->set_callback(client->fd_num(), EPOLLIN,
+        [epoller_weak = weak_ptr<Epoller>(epoller),
+         client_weak = weak_ptr<TCPSocket>(client)]() {
+          auto epoller = epoller_weak.lock();
+          assert(epoller);
 
-          TCPSocket & client = clients.at(client_fd);
+          auto client = client_weak.lock();
+          assert(client);
 
-          const string data = client.read();
+          cerr << "Epoller " << epoller->fd_num()
+               << ": EPOLLIN event from client " << client->fd_num() << endl;
+
+          const string data = client->read();
 
           if (data.empty()) {
-            epoller->deregister(client);
-            clients.erase(client.fd_num());
+            clients.erase(client->fd_num());
           } else {
-            cerr << client_fd << ": received " << data;
+            cerr << "Client " << client->fd_num() << ": received " << data;
 
             if (not pending_data.empty()) {
-              epoller->modify_events(client, EPOLLIN | EPOLLOUT);
+              epoller->modify_events(client->fd_num(), EPOLLIN | EPOLLOUT);
             }
           }
 
@@ -82,23 +86,23 @@ int main()
         }
       );
 
-      epoller->set_callback(client, EPOLLOUT,
-        [epoller_weak_ptr = weak_ptr<Epoller>(epoller), client_fd]() {
-          auto epoller = epoller_weak_ptr.lock();
-          if (not epoller) {
-            cerr << "Epoller is gone" << endl;
-            return -1;
-          }
+      epoller->set_callback(client->fd_num(), EPOLLOUT,
+        [epoller_weak = weak_ptr<Epoller>(epoller),
+         client_weak = weak_ptr<TCPSocket>(client)]() {
+          auto epoller = epoller_weak.lock();
+          assert(epoller);
 
-          cerr << "EPOLLOUT event from client " << client_fd << endl;
+          auto client = client_weak.lock();
+          assert(client);
 
-          TCPSocket & client = clients.at(client_fd);
+          cerr << "Epoller " << epoller->fd_num()
+               << ": EPOLLOUT event from client " << client->fd_num() << endl;
 
-          client.write(pending_data.front());
+          client->write(pending_data.front());
           pending_data.pop_front();
 
           if (pending_data.empty()) {
-            epoller->modify_events(client, EPOLLIN);
+            epoller->modify_events(client->fd_num(), EPOLLIN);
           }
 
           return 0;
