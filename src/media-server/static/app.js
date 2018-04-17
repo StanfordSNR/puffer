819e6a770d91c72ab479a6af208b09b707d3899a
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-17 11:23:18 -0700

    Stop streaming after user is logged out

--- src/media-server/static/app.js
+++ src/media-server/static/app.js
@@ -489,42 +489,20 @@ function start_streaming() {
   client.connect();
 };
 
-function getWidgetUrl() {
-  return '/widget.html';
-}
-
-function handleSignedInUser(user) {
-  document.getElementById('user-signed-in').style.display = 'block';
-  document.getElementById('user-signed-out').style.display = 'none';
-  document.getElementById('user-prompt').textContent = 'Welcome! ' + user.displayName;
-
-  /* Start streaming if user is logged in */
-  start_streaming();
-}
-
-function handleSignedOutUser(user) {
-  document.getElementById('user-signed-in').style.display = 'none';
-  document.getElementById('user-signed-out').style.display = 'block';
-  document.getElementById('user-prompt').textContent = 'Please sign in';
-}
-
 function initApp() {
-  // Listening for auth state changes.
+  /* Listening for auth state changes */
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      // User is signed in.
-      handleSignedInUser(user);
+      /* User is signed in */
+      document.getElementById('user-signed-in').style.display = 'block';
+      document.getElementById('user-info').textContent = 'Welcome! ' + user.displayName;
+      /* Start streaming */
+      start_streaming();
     } else {
-      // User is signed out.
-      handleSignedOutUser(user);
+      /* Redirect to the sign-in page if user is not signed in */
+      window.location.replace('/widget.html');
     }
   });
-
-  document.getElementById('sign-in').addEventListener('click',
-    function() {
-      window.location.assign(getWidgetUrl());
-    }
-  );
 
   document.getElementById('sign-out').addEventListener('click',
     function() {
