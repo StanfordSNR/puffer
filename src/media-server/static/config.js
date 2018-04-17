6c5e694babe513bb5832ddd62ac8b3a7483a9014
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-17 10:20:57 -0700

    Add Firebase authentication widget

---
+++ src/media-server/static/config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+var config = {
+  apiKey: "AIzaSyCjOaVTCqaAy0dQMndAp0elms5rsz7Zl2w",
+  authDomain: "puffer-stanford.firebaseapp.com",
+  databaseURL: "https://puffer-stanford.firebaseio.com",
+  projectId: "puffer-stanford",
+  storageBucket: "puffer-stanford.appspot.com",
+  messagingSenderId: "696405620004"
+};
+firebase.initializeApp(config);
