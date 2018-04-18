082cc8095524ed2a7e32f7301e9c0d2245b7c2f1
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-18 10:37:17 -0700

    ws_message_parser.cc: repeatedly parse complete frames

--- src/net/ws_message_parser.cc
+++ src/net/ws_message_parser.cc
@@ -7,52 +7,56 @@ using namespace std;
 void WSMessageParser::parse(const string & buf)
 {
   raw_buffer_.append(buf);
-  uint64_t expected_length = WSFrame::expected_length(raw_buffer_);
 
-  if (raw_buffer_.length() < expected_length) {
-    /* still need more bytes to have a complete frame */
-    return;
-  }
+  /* repeatedly parse complete frames */
+  while (not raw_buffer_.empty()) {
+    uint64_t expected_length = WSFrame::expected_length(raw_buffer_);
 
-  /* okay, we have a complete frame now! */
-  WSFrame frame {raw_buffer_.substr(0, expected_length)};
-  raw_buffer_.erase(0, expected_length);
-
-  switch (frame.header().opcode()) {
-  case WSFrame::OpCode::Continuation:
-    if (frame_buffer_.size() == 0) {
-      throw runtime_error("message cannot start with a continuation frame");
+    if (raw_buffer_.length() < expected_length) {
+      /* still need more bytes to have a complete frame */
+      return;
     }
 
-    frame_buffer_.emplace_back(move(frame));
-    break;
+    /* okay, we have a complete frame now! */
+    WSFrame frame {raw_buffer_.substr(0, expected_length)};
+    raw_buffer_.erase(0, expected_length);
 
-  case WSFrame::OpCode::Text:
-  case WSFrame::OpCode::Binary:
-    if (frame_buffer_.size() != 0) {
-      throw runtime_error("expected a continuation message, got text/binary");
+    switch (frame.header().opcode()) {
+    case WSFrame::OpCode::Continuation:
+      if (frame_buffer_.size() == 0) {
+        throw runtime_error("message cannot start with a continuation frame");
+      }
+
+      frame_buffer_.emplace_back(move(frame));
+      break;
+
+    case WSFrame::OpCode::Text:
+    case WSFrame::OpCode::Binary:
+      if (frame_buffer_.size() != 0) {
+        throw runtime_error("expected a continuation message, got text/binary");
+      }
+
+      frame_buffer_.emplace_back(move(frame));
+      break;
+
+    case WSFrame::OpCode::Close:
+    case WSFrame::OpCode::Ping:
+    case WSFrame::OpCode::Pong:
+      if (not frame.header().fin()) {
+        throw runtime_error("control frames must not be fragmented");
+      }
+
+      /* we don't put control frames into the frame buffer, we directly create
+      the message from those and push them into the output queue */
+      complete_messages_.emplace(list<WSFrame>{frame});
+      return;
+    default:
+      throw runtime_error("invalid opcode");
     }
 
-    frame_buffer_.emplace_back(move(frame));
-    break;
-
-  case WSFrame::OpCode::Close:
-  case WSFrame::OpCode::Ping:
-  case WSFrame::OpCode::Pong:
-    if (not frame.header().fin()) {
-      throw runtime_error("control frames must not be fragmented");
+    if (frame.header().fin()) {
+      complete_messages_.emplace(frame_buffer_);
+      frame_buffer_.clear();
     }
-
-    /* we don't put control frames into the frame buffer, we directly create
-    the message from those and push them into the output queue */
-    complete_messages_.emplace(list<WSFrame>{frame});
-    return;
-  default:
-    throw runtime_error("invalid opcode");
-  }
-
-  if (frame.header().fin()) {
-    complete_messages_.emplace(frame_buffer_);
-    frame_buffer_.clear();
   }
 }
