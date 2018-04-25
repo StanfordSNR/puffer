8b062e574d40984bf0771309a56b34989e7b5e59
Francis Y. Yan <fyy@cs.stanford.edu>
2018-04-25 16:56:27 -0700

    Refactor time.cc and time.test

--- src/time/time.cc
+++ src/time/time.cc
@@ -1,40 +1,46 @@
+#include <getopt.h>
 #include <time.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <iostream>
-#include <vector>
-#include <chrono>
 
-#include "path.hh"
-#include "strict_conversions.hh"
+#include <iostream>
+#include <string>
+
 #include "file_descriptor.hh"
-#include "tokenize.hh"
+#include "filesystem.hh"
 #include "exception.hh"
 
 using namespace std;
 
-const int default_timescale = 90000;
-const size_t buffer_size = 64;
+static const uint32_t global_timescale = 90000;
+static const char time_format[] = "%Y-%m-%dT%H:%M:%SZ";
 
 void print_usage(const string & program_name)
 {
   cerr <<
-  "Usage: " << program_name << " [options] <seg> <seg>...\n"
-  "-o --output <output>    program will write the timefile to <output>.\n"
-  "-s --scale <timescale>  presentation time scale.\n"
-  "-d --delay <time>       delay presentation time by <time> seconds.\n"
-  "<seg>                   media segment list. Only the first one will be\n"
-  "                        used."
+  "Usage: " << program_name << " [options] <input_segment>\n\n"
+  "<input_segment>    input media segment named timestamp.xxx\n\n"
+  "Options:\n"
+  "-o --output    file to output the clock time\n"
+  "-d --delay     delay presentation time by <time> seconds"
   << endl;
 }
 
-string format_time(const time_t time)
+/* convert string to time_t */
+time_t parse_time(const string & time_str)
 {
-  char buf[buffer_size];
-  tm now_tm;
-  gmtime_r(&time, &now_tm);
-  strftime(buf, sizeof buf, "%Y-%m-%dT%H:%M:%SZ", &now_tm);
+  struct tm tm;
+  strptime(time_str.c_str(), time_format, &tm);
+  return timegm(&tm);
+}
+
+/* convert time_t to string */
+string format_time(const time_t t)
+{
+  struct tm tm;
+  gmtime_r(&t, &tm);
+
+  char buf[64];
+  strftime(buf, sizeof(buf), time_format, &tm);
   return buf;
 }
 
@@ -44,31 +50,26 @@ int main(int argc, char * argv[])
     abort();
   }
 
-  string timescale, input, output, delay;
+  string output_timefile, presentation_delay;
 
   const option cmd_line_opts[] = {
     {"output", required_argument, nullptr, 'o'},
-    {"scale",  required_argument, nullptr, 's'},
     {"delay",  required_argument, nullptr, 'd'},
     { nullptr, 0,                 nullptr,  0 },
   };
 
   while (true) {
-    const int opt = getopt_long(argc, argv, "o:s:d:", cmd_line_opts,
-                                nullptr);
+    const int opt = getopt_long(argc, argv, "o:d:", cmd_line_opts, nullptr);
     if (opt == -1) {
       break;
     }
 
     switch (opt) {
     case 'o':
-      output = optarg;
-      break;
-    case 's':
-      timescale = optarg;
+      output_timefile = optarg;
       break;
     case 'd':
-      delay = optarg;
+      presentation_delay = optarg;
       break;
     default:
       print_usage(argv[0]);
@@ -76,74 +77,46 @@ int main(int argc, char * argv[])
     }
   }
 
-  if (optind >= argc) {
+  if (optind != argc - 1) {
     print_usage(argv[0]);
     return EXIT_FAILURE;
   }
 
-  if (output.empty()) {
-    cout << "Missing -o <output>" << endl;
+  string input_segment = argv[optind];
+
+  if (output_timefile.empty()) {
+    cerr << "Error: -o is required" << endl;
     print_usage(argv[0]);
     return EXIT_FAILURE;
   }
 
-  input = argv[optind];
+  string timestamp_str = fs::path(input_segment).stem();
+  int64_t timestamp = stoll(timestamp_str);
 
-  string time_string = roost::rbasename(input).string();
-  time_string = split_filename(time_string).first;
-  long long i_time = stoll(time_string);
-
-  long long i_timescale;
-  if (timescale.size()) {
-    i_timescale = stoll(timescale);
-  } else {
-    i_timescale = default_timescale;
+  /* duration in seconds */
+  time_t duration = timestamp / global_timescale;
+  if (not presentation_delay.empty()) {
+    duration -= stoi(presentation_delay);
   }
 
-  long long i_delay;
-  if (delay.size()) {
-    i_delay = stoll(delay);
-  } else {
-    i_delay = 0;
+  /* write to timefile if input timestamp is greater than the existing one */
+  FileDescriptor fd(CheckSystemCall("open (" + output_timefile + ")",
+      open(output_timefile.c_str(), O_RDWR | O_CREAT, 0644)));
+  fd.acquire_exclusive_flock();
+
+  time_t curr_duration = -1;
+  string time_str = fd.read();
+  if (not time_str.empty()) {
+    curr_duration = parse_time(time_str);
   }
 
-  /* 1. obtain the lock for the output
-   * 2. compare the time file time and input time
-   * 3. if input time is >= the time file, flush the time file
-   * 4. release the lock
-   */
-  FileDescriptor fd(CheckSystemCall("open (" + output + ")",
-         open(output.c_str(), O_RDWR | O_CREAT, 0644)));
-  fd.block_for_exclusive_lock();
-
-  uint64_t filesize = fd.filesize();
-
-  /* actual duration in seconds */
-  uint32_t actual_duration = i_time / i_timescale;
-  actual_duration = actual_duration > i_delay ?
-                    actual_duration - i_delay : 0;
-
-  time_t duration = narrow_cast<time_t>(actual_duration);
-
-  if (filesize > 0) {
-    string file_time = fd.read(buffer_size);
-    /* convert to time_t */
-    struct tm tm;
-    strptime(file_time.c_str(), "%FT%TZ", &tm);
-    time_t t = timegm(&tm);
-    if (duration <= t) {
-      /* no need to update */
-      fd.release_flock();
-      fd.close();
-      return EXIT_SUCCESS;
-    }
+  if (curr_duration == -1 or duration > curr_duration) {
+    fd.reset_offset();
+    fd.write(format_time(duration));
   }
 
-  string updated_time = format_time(duration);
-  /* reset the file content */
-  fd.reset();
-  fd.write(updated_time);
   fd.release_flock();
   fd.close();
+
   return EXIT_SUCCESS;
 }
