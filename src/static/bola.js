aa2ad936d65815d60eef7a714303a68ae0ce2e00
Francis Y. Yan <fyy@cs.stanford.edu>
2018-05-10 11:44:29 -0700

    Replace pensieve_bola with the BOLA built in dash.js

---
+++ src/static/bola.js
@@ -0,0 +1,32 @@
+const manifest_url = '/media/tos/ready/live.mpd'
+
+function start_streaming(user) {
+  var player = dashjs.MediaPlayer().create();
+  player.initialize(document.getElementById("tv-player"), manifest_url, true);
+  player.setABRStrategy('abrBola');
+  player.clearDefaultUTCTimingSources();
+}
+
+function init_app() {
+  /* Listening for auth state changes */
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      document.getElementById('user-signed-in').style.display = 'block';
+      document.getElementById('user-info').textContent = 'Welcome! ' + user.displayName;
+
+      /* User is signed in */
+      start_streaming(user);
+    } else {
+      /* Redirect to the sign-in page if user is not signed in */
+      window.location.replace('/widget.html');
+    }
+  });
+
+  document.getElementById('sign-out').addEventListener('click',
+    function() {
+      firebase.auth().signOut();
+    }
+  );
+}
+
+window.addEventListener('load', init_app);
