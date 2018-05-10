9f18dd6fc95a4f5b0dd6712511a2b047cd5037c2
Francis Y. Yan <fyy@cs.stanford.edu>
2018-05-10 15:13:45 -0700

    Allow for not specifying --media if static/media is already a symlink

--- src/static/run_servers.py
+++ src/static/run_servers.py
@@ -7,9 +7,16 @@ import argparse
 from subprocess import Popen
 
 
+def check_before_link(p):
+    if path.exists(p) and not path.islink(p):
+        sys.exit('Error: {} already exists but is not a symlink'
+                 .format(p))
+
+
 def link_dist(base_dir):
     src_dist = path.join(base_dir, 'third_party', 'dist-for-puffer')
     dst_dist = path.join(base_dir, 'src', 'static', 'dist')
+    check_before_link(dst_dist)
 
     if not path.islink(dst_dist) or os.readlink(dst_dist) != src_dist:
         os.symlink(src_dist, dst_dist)
@@ -19,28 +26,34 @@ def link_dist(base_dir):
 
 def link_media(base_dir, src_media):
     dst_media = path.join(base_dir, 'src', 'static', 'media')
+    check_before_link(dst_media)
 
-    if not path.islink(dst_media) or os.readlink(dst_media) != src_media:
-        os.symlink(src_media, dst_media)
-        sys.stderr.write('Created symlink {} -> {}\n'
-                         .format(src_media, dst_media))
+    if src_media is None:
+        if not path.islink(dst_media):
+            sys.exit('static/media must be a symlink if not specify --media')
+    else:
+        src_media = path.abspath(src_media)
+        if not path.islink(dst_media) or os.readlink(dst_media) != src_media:
+            os.symlink(src_media, dst_media)
+            sys.stderr.write('Created symlink {} -> {}\n'
+                             .format(src_media, dst_media))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('servers_config', help='YAML config file for servers')
-    parser.add_argument('--media', required=True,
+    parser.add_argument('--media',
             help='create a symlink to the directory containing channel media')
     args = parser.parse_args()
 
     base_dir = path.abspath(path.join(path.dirname(__file__),
                                       os.pardir, os.pardir))
 
-    # create a symbolic link for dist-for-puffer
+    # check and create a symbolic link for dist-for-puffer
     link_dist(base_dir)
 
-    # create a symbolic link for media
-    link_media(base_dir, path.abspath(args.media))
+    # check or create a symbolic link for media
+    link_media(base_dir, args.media)
 
     procs = []
 
