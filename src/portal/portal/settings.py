01d24f5289bfe6a4612d623d2be5e1b261bc17a3
hudson-ayers <hayers@stanford.edu>
2018-05-29 19:14:10 -0700

    Modify settings.py to provide reference to app templates and urls

--- src/portal/portal/settings.py
+++ src/portal/portal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'puffer.apps.PufferConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
