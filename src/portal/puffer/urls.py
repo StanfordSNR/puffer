dd7ec7c5a9bcad09e36bcb548a11ff3504031704
hudson-ayers <hayers@stanford.edu>
2018-05-29 21:31:41 -0700

    Fix template paths to reference correct html files

--- src/portal/puffer/urls.py
+++ src/portal/puffer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('index_dev.html', TemplateView.as_view(template_name='index_dev.html')),
-    path('player.html', TemplateView.as_view(template_name='player.html')),
+    path('', TemplateView.as_view(template_name='puffer/index.html')),
+    path('index_dev.html', TemplateView.as_view(template_name='puffer/index_dev.html')),
+    path('player.html', TemplateView.as_view(template_name='puffer/player.html')),
 ]
