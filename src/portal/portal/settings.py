986617a629b42ea5280e882f1789ac094350c0a4
hudson-ayers <hayers@stanford.edu>
2018-05-29 21:29:13 -0700

    Add login redirect url to settings.py

--- src/portal/portal/settings.py
+++ src/portal/portal/settings.py
@@ -119,3 +119,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
