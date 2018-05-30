85a41140711818725434601d0122263b7f71b9c8
hudson-ayers <hayers@stanford.edu>
2018-05-29 19:21:53 -0700

    Modify index files to point to appropriate static files, and remove code that was used for firebase

--- src/portal/puffer/static/puffer/index.js
+++ src/portal/puffer/static/puffer/index.js
@@ -1,15 +1,15 @@
 function rand_alg() {
   var rand_urls = [
-    "/player.html?aid=1",
-    "/player.html?aid=2",
-    "/player.html?aid=3",
-    "/player.html?aid=4",
-    "/player.html?aid=5",
-    "/player.html?aid=6",
-    "/player.html?aid=7",
-    "/player.html?aid=8",
-    "/player.html?aid=10",
-    "/player.html?aid=11"
+    "../player.html?aid=1",
+    "../player.html?aid=2",
+    "../player.html?aid=3",
+    "../player.html?aid=4",
+    "../player.html?aid=5",
+    "../player.html?aid=6",
+    "../player.html?aid=7",
+    "../player.html?aid=8",
+    "../player.html?aid=10",
+    "../player.html?aid=11"
   ];
 
   var rand_url = rand_urls[Math.floor(Math.random() * rand_urls.length)];
@@ -17,25 +17,5 @@ function rand_alg() {
 }
 
 function init_app() {
-  /* Listening for auth state changes */
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      /* User is signed in */
-      document.getElementById('user-signed-in').style.display = 'block';
-      document.getElementById('user-info').textContent = 'Welcome! ' + user.displayName;
 
-      document.getElementById('play-rand-alg').addEventListener('click', rand_alg);
-    } else {
-      /* Redirect to the sign-in page if user is not signed in */
-      window.location.replace('/widget.html');
-    }
-  });
-
-  document.getElementById('sign-out').addEventListener('click',
-    function() {
-      firebase.auth().signOut();
-    }
-  );
 }
-
-window.addEventListener('load', init_app);
