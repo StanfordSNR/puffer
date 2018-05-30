fa8c089dc7feecae9c4492cb98ff9bb83d02fade
hudson-ayers <hayers@stanford.edu>
2018-05-30 02:08:56 -0700

    Switch from mysql database to postgres database

--- src/portal/portal/settings.py
+++ src/portal/portal/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'puffer_django2',
+        'USER': 'pufferuser2',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
