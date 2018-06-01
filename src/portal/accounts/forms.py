e36631aa5d30c36ac460aa4d99ab18ecada93a18
hudson-ayers <hayers@stanford.edu>
2018-06-01 00:32:15 -0700

    No longer store or ask for users full name

--- src/portal/accounts/forms.py
+++ src/portal/accounts/forms.py
@@ -14,10 +14,6 @@ class InviteTokenField(forms.CharField):
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False,
-                                 help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False,
-                                help_text='Optional.')
     email = forms.EmailField(max_length=254,
                              help_text='Required. Inform a valid\
                                         email address.')
@@ -27,5 +23,5 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name',
-                  'email', 'invite_token', 'password1', 'password2', )
+        fields = ('username', 'email', 'invite_token', 'password1',
+                  'password2', )
