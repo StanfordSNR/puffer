605d3aad47d97d2b5a093edcfb38af61bc33df55
hudson-ayers <hayers@stanford.edu>
2018-06-01 19:13:53 -0700

    Admin users can assign tokens to other users via the admin interface

--- src/portal/accounts/admin.py
+++ src/portal/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import InvitationToken
 
-# Register your models here.
+
+admin.site.register(InvitationToken)
--- src/portal/accounts/models.py
+++ src/portal/accounts/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class InvitationToken(models.Model):
     token = models.CharField(max_length=64, default=None)
+    holder = models.ForeignKey(User, on_delete=models.CASCADE,
+                               null=True, blank=True)
 
-# To add tokens log directly into the postgres database and add tokens
+    def __str__(self):
+        holder = "Unassigned"
+        admin = ""
+        if self.holder:
+            holder = self.holder.email
+            admin = self.holder.is_superuser
+            if admin:
+                return self.token + " (" + holder + "- admin)"
+            else:
+                return self.token + " (" + holder + " - non-admin)"
+        return self.token + " (" + holder + ")"
