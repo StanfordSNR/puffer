5d69a2cea4ecd301ce227160ff104acac72523fa
Francis Y. Yan <fyy@cs.stanford.edu>
2018-06-04 12:54:54 -0700

    Use Argon2 as the default password hashing algorithm

--- src/portal/portal/settings.py
+++ src/portal/portal/settings.py
@@ -104,6 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Password hashers
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
