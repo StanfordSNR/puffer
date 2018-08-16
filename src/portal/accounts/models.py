4b00989388705d477545374bcb81f7c61fd45f79
Francis Y. Yan <fyy@cs.stanford.edu>
2018-08-15 23:27:49 -0700

    Add tokens that can be expanded to more when registered with

--- src/portal/accounts/models.py
+++ src/portal/accounts/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 
 
 class InvitationToken(models.Model):
-    token = models.CharField(max_length=64)
+    token = models.CharField(max_length=64, unique=True)
     holder = models.ForeignKey(User, on_delete=models.CASCADE,
                                null=True, blank=True)
+    # when a user registers an account with this token, the new account will
+    # be assigned and hold "addon_cnt" extra tokens for distribution
+    addon_cnt = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        if self.holder:
-            return self.token + " (" + self.holder.username + ")"
-        else:
-            return self.token + " (unassigned)"
+        holder = self.holder.username if self.holder else 'unassigned'
+        return '%s (%s, %d)' % (self.token, holder, self.addon_cnt)
--- src/portal/accounts/views.py
+++ src/portal/accounts/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth import views as auth_views
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
 from accounts.models import InvitationToken
 from accounts.forms import SignUpForm
+from accounts.utils import random_token
 
 
 def my_login(request):
@@ -14,8 +16,7 @@ def my_login(request):
 
     if request.method == 'POST':
         if 'america' not in request.POST:
-            messages.error(
-                request, 'Puffer is only available in the United States.')
+            messages.error(request, 'Puffer is only available in the US.')
 
             redirect_to = request.POST.get('next')
             if redirect_to:
@@ -35,13 +36,24 @@ def signup(request):
         form = SignUpForm(request.POST)
 
         if form.is_valid():
-            form.save()
-
-            # delete invitation token after the new user is created
             invite_token = form.cleaned_data.get('invite_token')
-            matching_token = InvitationToken.objects.filter(token=invite_token)
-            if matching_token:
-                matching_token.delete()
+            try:
+                matching_token = InvitationToken.objects.get(token=invite_token)
+            except ObjectDoesNotExist:
+                messages.error(request, 'Failed to validate the provided token.')
+                return redirect('signup')
+
+            # save the user if the token is validated
+            user = form.save()
+
+            # create "addon_cnt" tokens with addon_cnt=0
+            if matching_token.addon_cnt:
+                for _ in range(matching_token.addon_cnt):
+                    InvitationToken.objects.create(
+                            token=random_token(), holder=user)
+
+            # delete used tokens
+            matching_token.delete()
 
             messages.success(request,
                 'Your account has been created successfully! Please log in.')
