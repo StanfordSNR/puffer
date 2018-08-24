426bbd27ca539808b5491fc1e08a8a428e3a6e30
Francis Y. Yan <fyy@cs.stanford.edu>
2018-08-23 17:14:39 -0700

    Add favicon

--- src/portal/puffer/urls.py
+++ src/portal/puffer/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('favicon.ico', RedirectView.as_view(
+         url=staticfiles_storage.url('puffer/dist/images/favicon.ico')),
+         name='favicon'),
     path('profile/', views.profile, name='profile'),
     path('player/<int:aid>/', views.player, name='player'),
     path('rating/', views.rating, name='rating'),
