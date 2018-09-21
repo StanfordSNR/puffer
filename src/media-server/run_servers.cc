7d43b52aca6e4df98569b1664b7451d53b4929d6
Chenzhi Zhu <zhucz16@mails.tsinghua.edu.cn>
2018-09-21 12:29:48 +0800

    run_server.cc: post alert to influxdb when puffer servers or log reporters crash

--- src/media-server/run_servers.cc
+++ src/media-server/run_servers.cc
@@ -5,6 +5,8 @@
 #include "filesystem.hh"
 #include "path.hh"
 #include "child_process.hh"
+#include "influxdb_client.hh"
+#include "util.hh"
 
 using namespace std;
 
@@ -49,16 +51,38 @@ int main(int argc, char * argv[])
 
   ProcessManager proc_manager;
 
+  /* add influx client for posting states of child processes */
+  InfluxDBClient influxdb_client(proc_manager.poller(), {"127.0.0.1", 8086},
+                                 "collectd", "puffer",
+                                 safe_getenv("INFLUXDB_PASSWORD"));
+
   /* run multiple instances of ws_media_server */
   for (int i = 0; i < num_servers; i++) {
     vector<string> args { ws_media_server, yaml_config, to_string(i) };
-    proc_manager.run_as_child(ws_media_server, args);
+    proc_manager.run_as_child(ws_media_server, args, {},
+      [&influxdb_client](const pid_t & pid)  // error callback
+      {
+        cerr << "Error in media server: pid " << to_string(pid) << endl;
+        influxdb_client.post("server_state,pid=" + to_string(pid)
+                             + " state=\"error\" "
+                             + to_string(time(nullptr)));
+      }
+    );
 
     for (const auto & log_stem : log_stems) {
       string log_format = log_dir / (log_stem + ".conf");
       string log_path = log_dir / (log_stem + "." + to_string(i) + ".log");
+
       vector<string> log_args {log_reporter, log_format, log_path};
-      proc_manager.run_as_child(log_reporter, log_args);
+      proc_manager.run_as_child(log_reporter, log_args, {},
+        [&influxdb_client](const pid_t & pid)  // error callback
+        {
+          cerr << "Error in log reporter: pid " << to_string(pid) << endl;
+          influxdb_client.post("log_reporter_state,pid=" + to_string(pid)
+                               + " state=\"error\" "
+                               + to_string(time(nullptr)));
+        }
+      );
     }
   }
 
