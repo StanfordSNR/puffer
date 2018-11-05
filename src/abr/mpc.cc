de0ccdf42a1ce64fb7127204e1a1703c2c3d2908
Chenzhi Zhu <zhucz16@mails.tsinghua.edu.cn>
2018-11-05 21:30:28 +0800

    Port old_mpc.{hh,cc} to mpc.{hh,cc}

--- src/abr/mpc.cc
+++ src/abr/mpc.cc
@@ -3,12 +3,27 @@
 
 using namespace std;
 
+static const double HIGH_SENDING_TIME = 10;
+
 MPC::MPC(const WebSocketClient & client,
          const string & abr_name, const YAML::Node & abr_config)
-  : ABRAlgo(client, abr_name), past_chunks_()
+  : ABRAlgo(client, abr_name)
 {
-  if (abr_config["past_chunk_cnt"]) {
-    past_chunk_cnt_ = abr_config["past_chunk_cnt"].as<size_t>();
+  max_lookahead_horizon_ = abr_config["max_lookahead_horizon"] ?
+      min(MAX_LOOKAHEAD_HORIZON, abr_config["max_lookahead_horizon"].as<size_t>()) :
+      MAX_LOOKAHEAD_HORIZON;
+  dis_buf_length_ = abr_config["dis_buf_length"] ?
+      min(MAX_DIS_BUF_LENGTH, abr_config["dis_buf_length"].as<size_t>()) :
+      MAX_DIS_BUF_LENGTH;
+  rebuffer_length_coeff_ = abr_config["rebuffer_length_coeff"] ?
+      abr_config["rebuffer_length_coeff"].as<double>() : REBUFFER_LENGTH_COEFF;
+  ssim_diff_coeff_ = abr_config["ssim_diff_coeff"] ?
+      abr_config["ssim_diff_coeff"].as<double>() : SSIM_DIFF_COEFF;
+
+  unit_buf_length_ = WebSocketClient::MAX_BUFFER_S / dis_buf_length_;
+
+  for (size_t i = 0; i <= dis_buf_length_; i++) {
+    real_buffer_[i] = i * unit_buf_length_;
   }
 }
 
@@ -30,8 +45,124 @@ void MPC::video_chunk_acked(const VideoFormat & format,
 
 VideoFormat MPC::select_video_format()
 {
+  reinit();
+  size_t ret_format = update_value(0, curr_buffer_, curr_format_);
+  return client_.channel()->vformats()[ret_format];
+}
+
+void MPC::reinit()
+{
+  curr_round_++;
+
   const auto & channel = client_.channel();
   const auto & vformats = channel->vformats();
-  // TODO
-  return vformats[0];
+  const unsigned int vduration = channel->vduration();
+  const uint64_t curr_ts = *client_.next_vts() - vduration;
+
+  chunk_length_ = (double) vduration / channel->timescale();
+  num_formats_ = vformats.size();
+  lookahead_horizon_ = min(max_lookahead_horizon_,
+                       (*channel->vready_frontier() - curr_ts) / vduration);
+
+  /* initial failed if there is no ready chunk ahead */
+  if (lookahead_horizon_ == 0) {
+    throw runtime_error("no ready chunk ahead");
+  }
+
+  curr_buffer_ = discretize_buffer(client_.video_playback_buf());
+
+  /* get the current format */
+  auto curr_format = *client_.curr_vq();
+  curr_format_ = 0;
+  for (size_t i = 0; i < num_formats_; i++) {
+    if (vformats[i] == curr_format) {
+      curr_format_ = i;
+      break;
+    }
+  }
+
+  /* init curr_ssims */
+  for (size_t i = 0; i <= lookahead_horizon_; i++) {
+    for (size_t j = 0; j < num_formats_; j++) {
+      curr_ssims_[i][j] = channel->vssim(vformats[j], curr_ts + vduration * i);
+    }
+  }
+
+  /* init curr_sending_time */
+  size_t num_past_chunks = past_chunks_.size();
+
+  auto it = past_chunks_.begin();
+  for (size_t i = 0; it != past_chunks_.end(); it++, i++) {
+    unit_sending_time_[i] = (double) it->trans_time / it->chunk_size / 1000;
+  }
+
+  for (size_t i = 0; i < lookahead_horizon_; i++) {
+    double tmp = 0;
+    for (size_t j = 0; j < num_past_chunks; j++) {
+      tmp += unit_sending_time_[i + j];
+    }
+
+    if (num_past_chunks != 0) {
+      unit_sending_time_[i + num_past_chunks] = tmp / num_past_chunks;
+    } else {
+      /* set the sending time to be a default hight value */
+      unit_sending_time_[i + num_past_chunks] = HIGH_SENDING_TIME;
+    }
+
+    const auto & data_map = channel->vdata(curr_ts + vduration * i);
+
+    for (size_t j = 0; j < num_formats_; j++) {
+      curr_sending_time_[i][j] = get<1>(data_map.at(vformats[j]))
+                                 * unit_sending_time_[i + num_past_chunks];
+    }
+  }
+}
+
+size_t MPC::update_value(size_t i, size_t curr_buffer, size_t curr_format)
+{
+  flag_[i][curr_buffer][curr_format] = curr_round_;
+
+  if (i == lookahead_horizon_) {
+    v_[i][curr_buffer][curr_format] = curr_ssims_[i][curr_format];
+    return 0;
+  }
+
+  size_t best_next_format = num_formats_;
+  double max_qvalue = 0;
+  for (size_t next_format = 0; next_format < num_formats_; next_format++) {
+    double qvalue = get_qvalue(i, curr_buffer, curr_format, next_format);
+    if (best_next_format == num_formats_ or qvalue > max_qvalue) {
+      max_qvalue = qvalue;
+      best_next_format = next_format;
+    }
+  }
+  v_[i][curr_buffer][curr_format] = max_qvalue;
+
+  return best_next_format;
+}
+
+double MPC::get_qvalue(size_t i, size_t curr_buffer, size_t curr_format,
+                       size_t next_format)
+{
+  double real_rebuffer = curr_sending_time_[i][next_format]
+                         - real_buffer_[curr_buffer];
+  double next_buffer = discretize_buffer(max(0.0, -real_rebuffer) + chunk_length_);
+  return curr_ssims_[i][curr_format]
+         - ssim_diff_coeff_ * fabs(curr_ssims_[i][curr_format]
+                                   - curr_ssims_[i+1][next_format])
+         - rebuffer_length_coeff_ * max(0.0, real_rebuffer)
+         + get_value(i + 1, next_buffer, next_format);
+}
+
+double MPC::get_value(size_t i, size_t curr_buffer, size_t curr_format)
+{
+  if (flag_[i][curr_buffer][curr_format] != curr_round_) {
+    update_value(i, curr_buffer, curr_format);
+  }
+  return v_[i][curr_buffer][curr_format];
+}
+
+size_t MPC::discretize_buffer(double buf) {
+  size_t dis_buffer = (buf + unit_buf_length_ * 0.5) / unit_buf_length_;
+  return max((size_t)0, min(dis_buf_length_, dis_buffer));
 }
--- src/abr/mpc.hh
+++ src/abr/mpc.hh
@@ -5,6 +5,13 @@
 
 #include <deque>
 
+static const size_t MAX_DIS_BUF_LENGTH = 100;
+static const size_t MAX_NUM_FORMATS = 20;
+static const size_t PAST_CHUNK_CNT = 5;
+static const size_t MAX_LOOKAHEAD_HORIZON = 10;
+static const double REBUFFER_LENGTH_COEFF = 5;
+static const double SSIM_DIFF_COEFF = 0.3;
+
 class MPC : public ABRAlgo
 {
 public:
@@ -19,9 +26,57 @@ public:
   VideoFormat select_video_format() override;
 
 private:
-  size_t past_chunk_cnt_ {5};
+  size_t past_chunk_cnt_ {PAST_CHUNK_CNT};
 
-  std::deque<Chunk> past_chunks_;
+  std::deque<Chunk> past_chunks_ {};
+
+  /* all the time durations are measured in sec */
+  size_t max_lookahead_horizon_ {};
+  size_t lookahead_horizon_ {};
+  double chunk_length_ {};
+  size_t dis_buf_length_ {};
+  double unit_buf_length_ {};
+  size_t num_formats_ {};
+  double rebuffer_length_coeff_ {};
+  double ssim_diff_coeff_ {};
+
+  /* for the current buffer length and quality */
+  size_t curr_buffer_ {};
+  size_t curr_format_ {};
+
+  /* for storing the value fucntion*/
+  uint64_t flag_[MAX_LOOKAHEAD_HORIZON + 1][MAX_DIS_BUF_LENGTH + 1][MAX_NUM_FORMATS] {};
+  double v_[MAX_LOOKAHEAD_HORIZON + 1][MAX_DIS_BUF_LENGTH + 1][MAX_NUM_FORMATS] {};
+
+  /* record the current dp round */
+  uint64_t curr_round_ {};
+
+  /* map the discretized buffer length to the estimation */
+  double real_buffer_[MAX_DIS_BUF_LENGTH + 1] {};
+
+  /* unit sending time estimation */
+  double unit_sending_time_[MAX_LOOKAHEAD_HORIZON + 1 + PAST_CHUNK_CNT] {};
+
+  /* the ssim of the chunk given the timestamp and format */
+  double curr_ssims_[MAX_LOOKAHEAD_HORIZON + 1][MAX_NUM_FORMATS] {};
+
+  /* the estimation of sending time given the timestamp and format */
+  double curr_sending_time_[MAX_LOOKAHEAD_HORIZON + 1][MAX_NUM_FORMATS] {};
+
+  void reinit();
+
+  /* calculate the value of curresponding state and return the best strategy */
+  size_t update_value(size_t i, size_t curr_buffer, size_t curr_format);
+
+  /* return the qvalue of the given cur state and next action */
+  double get_qvalue(size_t i, size_t curr_buffer, size_t curr_format,
+                    size_t next_format);
+
+  /* return the value of the given state */
+  double get_value(size_t i, size_t curr_buffer, size_t curr_format);
+
+  /* discretize the buffer length */
+  size_t discretize_buffer(double buf);
 };
 
 #endif /* MPC_HH */
