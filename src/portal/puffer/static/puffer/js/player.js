2e061dedc815b8d0029c801bef38505de38146de
Francis Y. Yan <fyy@cs.stanford.edu>
2018-11-07 21:23:30 -0800

    Move get_client_system_info from puffer.js to player.js as it is general

--- src/portal/puffer/static/puffer/js/player.js
+++ src/portal/puffer/static/puffer/js/player.js
@@ -221,6 +221,97 @@ function ChannelBar() {
   };
 }
 
+
+function get_client_system_info() {
+  /* Below code adapted from https://github.com/keithws/browser-report */
+  var nAgt = navigator.userAgent;
+  var browser = navigator.appName;
+  var nameOffset, verOffset;
+
+  // Opera
+  if ((verOffset = nAgt.indexOf('Opera')) != -1) {
+    browser = 'Opera';
+  }
+  // Opera Next
+  if ((verOffset = nAgt.indexOf('OPR')) != -1) {
+    browser = 'Opera';
+  }
+  // Edge
+  else if ((verOffset = nAgt.indexOf('Edge')) != -1) {
+    browser = 'Microsoft Edge';
+  }
+  // MSIE
+  else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
+    browser = 'Microsoft Internet Explorer';
+  }
+  // Chrome
+  else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
+    browser = 'Chrome';
+  }
+  // Safari
+  else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
+    browser = 'Safari';
+  }
+  // Firefox
+  else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
+    browser = 'Firefox';
+  }
+  // MSIE 11+
+  else if (nAgt.indexOf('Trident/') != -1) {
+    browser = 'Microsoft Internet Explorer';
+  }
+  // Other browsers
+  else if ((nameOffset = nAgt.lastIndexOf(' ') + 1)
+           < (verOffset = nAgt.lastIndexOf('/'))) {
+    browser = nAgt.substring(nameOffset, verOffset);
+    if (browser.toLowerCase() === browser.toUpperCase()) {
+      browser = navigator.appName;
+    }
+  }
+
+  // system
+  var os = 'unknown';
+  var clientStrings = [
+    {s:'Windows 10', r:/(Windows 10.0|Windows NT 10.0)/},
+    {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
+    {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
+    {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
+    {s:'Windows Vista', r:/Windows NT 6.0/},
+    {s:'Windows Server 2003', r:/Windows NT 5.2/},
+    {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
+    {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
+    {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
+    {s:'Windows 98', r:/(Windows 98|Win98)/},
+    {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
+    {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
+    {s:'Windows CE', r:/Windows CE/},
+    {s:'Windows 3.11', r:/Win16/},
+    {s:'Android', r:/Android/},
+    {s:'Open BSD', r:/OpenBSD/},
+    {s:'Sun OS', r:/SunOS/},
+    {s:'Linux', r:/(Linux|X11)/},
+    {s:'iOS', r:/(iPhone|iPad|iPod)/},
+    {s:'Mac OS X', r:/Mac OS X/},
+    {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
+    {s:'QNX', r:/QNX/},
+    {s:'UNIX', r:/UNIX/},
+    {s:'BeOS', r:/BeOS/},
+    {s:'OS/2', r:/OS\/2/},
+    {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+  ];
+  for (var id in clientStrings) {
+    var cs = clientStrings[id];
+    if (cs.r.test(nAgt)) {
+      os = cs.s;
+      break;
+    }
+  }
+  return {
+    os: os,
+    browser: browser
+  };
+}
+
 function init_player(params_json) {
   var params = JSON.parse(params_json);
 
@@ -234,6 +325,9 @@ function init_player(params_json) {
     return;
   }
 
+  /* client's system information (OS and browser) */
+  var sysinfo = get_client_system_info();
+
   var control_bar = new ControlBar();
   var channel_bar = new ChannelBar();
 
@@ -245,7 +339,8 @@ function init_player(params_json) {
 
   load_script('/static/puffer/js/puffer.js').onload = function() {
     /* start_puffer is defined in puffer.js */
-    var ws_client = start_puffer(session_key, username, settings_debug);
+    var ws_client = start_puffer(session_key, username,
+                                 sysinfo, settings_debug);
 
     channel_bar.on_channel_change = function(new_channel) {
       ws_client.set_channel(new_channel);
--- src/portal/puffer/static/puffer/js/puffer.js
+++ src/portal/puffer/static/puffer/js/puffer.js
@@ -376,7 +376,7 @@ function AVSource(ws_client, video, options) {
   };
 }
 
-function WebSocketClient(video, session_key, username) {
+function WebSocketClient(session_key, username, sysinfo) {
   var that = this;
 
   var ws = null;
@@ -386,27 +386,18 @@ function WebSocketClient(video, session_key, username) {
   var rc_backoff = BASE_RECONNECT_BACKOFF;
   var last_open = null;
 
-  var os = null;
-  var browser = null;
-
   this.send_client_init = function(ws, channel) {
     if (!(ws && ws.readyState === WS_OPEN)) {
       return;
     }
 
     try {
-      if (os === null || browser === null) {
-        const client_info = get_client_system_info();
-        os = client_info.os;
-        browser = client_info.browser;
-      }
-
       var msg = {
         sessionKey: session_key,
         userName: username,
         channel: channel,
-        os: os,
-        browser: browser,
+        os: sysinfo.os,
+        browser: sysinfo.browser,
         screenHeight: screen.height,
         screenWidth: screen.width
       };
@@ -622,102 +613,9 @@ function WebSocketClient(video, session_key, username) {
   debug_timer_helper();
 }
 
-function get_client_system_info() {
-  /* Below code adapted from https://github.com/keithws/browser-report */
-  var nAgt = navigator.userAgent;
-  var browser = navigator.appName;
-  var nameOffset, verOffset;
-
-  // Opera
-  if ((verOffset = nAgt.indexOf('Opera')) != -1) {
-    browser = 'Opera';
-  }
-  // Opera Next
-  if ((verOffset = nAgt.indexOf('OPR')) != -1) {
-    browser = 'Opera';
-  }
-  // Edge
-  else if ((verOffset = nAgt.indexOf('Edge')) != -1) {
-    browser = 'Microsoft Edge';
-  }
-  // MSIE
-  else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
-    browser = 'Microsoft Internet Explorer';
-  }
-  // Chrome
-  else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
-    browser = 'Chrome';
-  }
-  // Safari
-  else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
-    browser = 'Safari';
-  }
-  // Firefox
-  else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
-    browser = 'Firefox';
-  }
-  // MSIE 11+
-  else if (nAgt.indexOf('Trident/') != -1) {
-    browser = 'Microsoft Internet Explorer';
-  }
-  // Other browsers
-  else if ((nameOffset = nAgt.lastIndexOf(' ') + 1)
-           < (verOffset = nAgt.lastIndexOf('/'))) {
-    browser = nAgt.substring(nameOffset, verOffset);
-    if (browser.toLowerCase() === browser.toUpperCase()) {
-      browser = navigator.appName;
-    }
-  }
-
-  // system
-  var os = 'unknown';
-  var clientStrings = [
-    {s:'Windows 10', r:/(Windows 10.0|Windows NT 10.0)/},
-    {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
-    {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
-    {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
-    {s:'Windows Vista', r:/Windows NT 6.0/},
-    {s:'Windows Server 2003', r:/Windows NT 5.2/},
-    {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
-    {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
-    {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
-    {s:'Windows 98', r:/(Windows 98|Win98)/},
-    {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
-    {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
-    {s:'Windows CE', r:/Windows CE/},
-    {s:'Windows 3.11', r:/Win16/},
-    {s:'Android', r:/Android/},
-    {s:'Open BSD', r:/OpenBSD/},
-    {s:'Sun OS', r:/SunOS/},
-    {s:'Linux', r:/(Linux|X11)/},
-    {s:'iOS', r:/(iPhone|iPad|iPod)/},
-    {s:'Mac OS X', r:/Mac OS X/},
-    {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
-    {s:'QNX', r:/QNX/},
-    {s:'UNIX', r:/UNIX/},
-    {s:'BeOS', r:/BeOS/},
-    {s:'OS/2', r:/OS\/2/},
-    {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
-  ];
-  for (var id in clientStrings) {
-    var cs = clientStrings[id];
-    if (cs.r.test(nAgt)) {
-      os = cs.s;
-      break;
-    }
-  }
-  return {
-    os: os,
-    browser: browser
-  };
-}
-
-function start_puffer(session_key, username, settings_debug) {
+function start_puffer(session_key, username, sysinfo, settings_debug) {
   /* if DEBUG = True in settings.py, connect to non-secure WebSocket server */
   non_secure = settings_debug;
 
-  const video = document.getElementById('tv-video');
-
-  const client = new WebSocketClient(video, session_key, username);
-  return client;
+  return new WebSocketClient(session_key, username, sysinfo);
 }
