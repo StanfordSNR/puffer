cf9619997603b4a2aa903829ab714140a4e09ee3
Francis Y. Yan <fyy@cs.stanford.edu>
2018-11-28 18:08:40 -0800

    run_servers.cc: launch servers as specified in YAML

--- src/media-server/run_servers.cc
+++ src/media-server/run_servers.cc
@@ -17,11 +17,13 @@
 using namespace std;
 using namespace CryptoPP;
 
+static string yaml_config;
+static YAML::Node config;
+static fs::path src_path;  /* path to puffer/src directory */
+
 void print_usage(const string & program_name)
 {
-  cerr <<
-  "Usage: " << program_name << " <YAML configuration> <number of servers>"
-  << endl;
+  cerr << "Usage: " << program_name << " <YAML configuration>" << endl;
 }
 
 string sha256(const string & input)
@@ -35,88 +37,61 @@ string sha256(const string & input)
   return digest;
 }
 
-int retrieve_expt_id(const YAML::Node & config, const string & json_str)
+int retrieve_expt_id(const string & json_str)
 {
   /* compute SHA-256 */
   string hash = sha256(json_str);
   cerr << "Experiment checksum: " << hash << endl;
 
-  try {
-    /* connect to PostgreSQL */
-    string db_conn_str = postgres_connection_string(config["postgres_connection"]);
-    pqxx::connection db_conn(db_conn_str);
+  /* connect to PostgreSQL */
+  string db_conn_str = postgres_connection_string(config["postgres_connection"]);
+  pqxx::connection db_conn(db_conn_str);
 
-    /* create table if not exists */
-    pqxx::work create_table(db_conn);
-    create_table.exec("CREATE TABLE IF NOT EXISTS puffer_experiment "
-                      "(id SERIAL PRIMARY KEY,"
-                      " hash VARCHAR(64) UNIQUE NOT NULL,"
-                      " data jsonb);");
-    create_table.commit();
+  /* create table if not exists */
+  pqxx::work create_table(db_conn);
+  create_table.exec("CREATE TABLE IF NOT EXISTS puffer_experiment "
+                    "(id SERIAL PRIMARY KEY,"
+                    " hash VARCHAR(64) UNIQUE NOT NULL,"
+                    " data jsonb);");
+  create_table.commit();
 
-    /* prepare two statements */
-    db_conn.prepare("select_id",
-      "SELECT id FROM puffer_experiment WHERE hash = $1;");
-    db_conn.prepare("insert_json",
-      "INSERT INTO puffer_experiment (hash, data) VALUES ($1, $2) RETURNING id;");
+  /* prepare two statements */
+  db_conn.prepare("select_id",
+    "SELECT id FROM puffer_experiment WHERE hash = $1;");
+  db_conn.prepare("insert_json",
+    "INSERT INTO puffer_experiment (hash, data) VALUES ($1, $2) RETURNING id;");
 
-    pqxx::work db_work(db_conn);
+  pqxx::work db_work(db_conn);
 
-    /* try to fetch an existing row */
-    pqxx::result r = db_work.prepared("select_id")(hash).exec();
-    if (r.size() == 1 and r[0].size() == 1) {
-      /* the same hash already exists */
-      return r[0][0].as<int>();
-    }
-
-    /* insert if no record exists and return the ID of inserted row */
-    r = db_work.prepared("insert_json")(hash)(json_str).exec();
-    db_work.commit();
-    if (r.size() == 1 and r[0].size() == 1) {
-      return r[0][0].as<int>();
-    }
-  } catch (const exception & e) {
-    print_exception("retrieve_expt_id", e);
+  /* try to fetch an existing row */
+  pqxx::result r = db_work.prepared("select_id")(hash).exec();
+  if (r.size() == 1 and r[0].size() == 1) {
+    /* the same hash already exists */
+    return r[0][0].as<int>();
   }
 
-  return -1;
+  /* insert if no record exists and return the ID of inserted row */
+  r = db_work.prepared("insert_json")(hash)(json_str).exec();
+  db_work.commit();
+  if (r.size() == 1 and r[0].size() == 1) {
+    return r[0][0].as<int>();
+  }
+
+  throw runtime_error("No valid experiment ID returned from database");
 }
 
-int main(int argc, char * argv[])
+int manage_experiment(const int expt_id)
 {
-  if (argc < 1) {
-    abort();
-  }
-
-  if (argc != 3) {
-    print_usage(argv[0]);
-    return EXIT_FAILURE;
-  }
-
-  string yaml_config = fs::absolute(argv[1]);
-  YAML::Node config = YAML::LoadFile(yaml_config);
-
-  const auto & src_path = fs::canonical(fs::path(
-      roost::readlink("/proc/self/exe")).parent_path().parent_path());
-
-  const auto & expt_json = src_path / "media-server/expt_json.py";
-  string json_str = run(expt_json, {expt_json, yaml_config}, true).first;
-
-  /* upload JSON to and retrieve an experimental ID from PostgreSQL */
-  int expt_id = retrieve_expt_id(config, json_str);
-  cerr << "Experiment ID: " << expt_id << endl;
-
-  int num_servers = stoi(argv[2]);
-  if (num_servers <= 0) {
-    cerr << "<number of servers>: a positive integer is required" << endl;
-    return EXIT_FAILURE;
-  }
-
-  const bool enable_logging = config["enable_logging"].as<bool>();
-
   ProcessManager proc_manager;
 
-  /* create influxdb_client only if enable_logging is true */
+  const bool enable_logging = config["enable_logging"].as<bool>();
+  if (enable_logging) {
+    cerr << "Logging is enabled" << endl;
+  } else {
+    cerr << "Logging is disabled" << endl;
+  }
+
+  /* create an influxdb_client only if enable_logging is true */
   unique_ptr<InfluxDBClient> influxdb_client = nullptr;
   if (enable_logging) {
     /* add influx client for posting states of child processes */
@@ -129,55 +104,74 @@ int main(int argc, char * argv[])
         safe_getenv(influx["password"].as<string>()));
   }
 
-  /* run multiple instances of ws_media_server */
+  /* will run log reporters only if enable_logging is true */
+  auto log_reporter = src_path / "monitoring/log_reporter";
+  vector<string> log_stems {
+    "active_streams", "playback_buffer", "rebuffer_event", "rebuffer_rate",
+    "video_quality"};
+
+  /* run media servers in each experimental group */
   const auto & ws_media_server = src_path / "media-server/ws_media_server";
-  for (int i = 0; i < num_servers; i++) {
-    vector<string> args { ws_media_server, yaml_config, to_string(i) };
-    proc_manager.run_as_child(ws_media_server, args, {},
-      [&influxdb_client, i](const pid_t &)  // error callback
-      {
-        cerr << "Error in media server with ID " << i << endl;
-        if (influxdb_client) {
-          /* at least one media server have failed */
-          influxdb_client->post("server_state state=1i "
-                                + to_string(time(nullptr)));
+  const auto & expt_groups = config["experimental_groups"];
+  int server_id = 0;
+
+  for (YAML::const_iterator it = expt_groups.begin();
+       it != expt_groups.end(); ++it) {
+    int group_id = it->first.as<int>();
+    const auto & group_node = it->second;
+    int num_servers = group_node["num_servers"].as<int>();
+    cerr << "Launching " << num_servers << " servers in experimental group "
+         << group_id << endl;
+
+    for (int i = 0; i < num_servers; i++) {
+      server_id++;
+
+      /* run media server with ID of server_id */
+      vector<string> args { ws_media_server,
+                            yaml_config, to_string(server_id),
+                            to_string(expt_id), to_string(group_id)};
+      proc_manager.run_as_child(ws_media_server, args, {},
+        [&influxdb_client, server_id](const pid_t &)  // error callback
+        {
+          cerr << "Error in media server with ID " << server_id << endl;
+          if (influxdb_client) {
+            /* at least one media server have failed */
+            influxdb_client->post("server_state state=1i "
+                                  + to_string(time(nullptr)));
+          }
+        }
+      );
+
+      /* run log_reporters */
+      if (influxdb_client) {
+        fs::path log_dir = config["log_dir"].as<string>();
+
+        for (const auto & log_stem : log_stems) {
+          string log_format = log_dir / (log_stem + ".conf");
+          string log_path = log_dir / (log_stem + "." + to_string(server_id)
+                                       + ".log");
+
+          vector<string> log_args { log_reporter, yaml_config,
+                                    log_format, log_path };
+          proc_manager.run_as_child(log_reporter, log_args, {},
+            [&influxdb_client, log_stem](const pid_t &)  // error callback
+            {
+              cerr << "Error in log reporter: " << log_stem << endl;
+              /* at least one log reporter have failed */
+              influxdb_client->post("log_reporter_state state=1i "
+                                    + to_string(time(nullptr)));
+            }
+          );
         }
       }
-    );
+    }
   }
 
   /* more logging actions */
-  if (enable_logging and influxdb_client) {
+  if (influxdb_client) {
     /* indicate that media servers are running */
     influxdb_client->post("server_state state=0i "
                           + to_string(time(nullptr)));
-
-    /* run log reporters */
-    auto log_reporter = src_path / "monitoring/log_reporter";
-    fs::path log_dir = config["log_dir"].as<string>();
-    vector<string> log_stems {
-      "active_streams", "playback_buffer", "rebuffer_event", "rebuffer_rate",
-      "video_quality"};
-
-    /* run a log reporter for each ws_media_server instance */
-    for (int i = 0; i < num_servers; i++) {
-      for (const auto & log_stem : log_stems) {
-        string log_format = log_dir / (log_stem + ".conf");
-        string log_path = log_dir / (log_stem + "." + to_string(i) + ".log");
-
-        vector<string> log_args { log_reporter, yaml_config,
-                                  log_format, log_path };
-        proc_manager.run_as_child(log_reporter, log_args, {},
-          [&influxdb_client, log_stem](const pid_t &)  // error callback
-          {
-            cerr << "Error in log reporter: " << log_stem << endl;
-            /* at least one log reporter have failed */
-            influxdb_client->post("log_reporter_state state=1i "
-                                  + to_string(time(nullptr)));
-          }
-        );
-      }
-    }
 
     /* indicate that log reporters are running */
     influxdb_client->post("log_reporter_state state=0i "
@@ -185,4 +179,39 @@ int main(int argc, char * argv[])
   }
 
   return proc_manager.wait();
+}
+
+int main(int argc, char * argv[])
+{
+  if (argc < 1) {
+    abort();
+  }
+
+  if (argc != 2) {
+    print_usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  /* save as global variables */
+  yaml_config = fs::absolute(argv[1]);
+  config = YAML::LoadFile(yaml_config);
+  src_path = fs::canonical(fs::path(
+      roost::readlink("/proc/self/exe")).parent_path().parent_path());
+
+  /* run expt_json.py to represent experimental settings as a JSON string */
+  const auto & expt_json = src_path / "media-server/expt_json.py";
+  string json_str = run(expt_json, {expt_json, yaml_config}, true).first;
+
+  /* upload JSON to and retrieve an experimental ID from PostgreSQL */
+  int expt_id;
+  try {
+    expt_id = retrieve_expt_id(json_str);
+  } catch (const exception & e) {
+    print_exception("retrieve_expt_id", e);
+    return EXIT_FAILURE;
+  }
+  cerr << "Experiment ID: " << expt_id << endl;
+
+  /* manage the experiment expt_id by running media servers as specified */
+  return manage_experiment(expt_id);
 }
