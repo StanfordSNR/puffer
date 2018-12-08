fac58e376f1408090e47c853ed5e7b93b112c48f
Francis Y. Yan <fyy@cs.stanford.edu>
2018-12-07 17:16:04 -0800

    Make profile/ and participate/ not accessible

--- src/portal/puffer/urls.py
+++ src/portal/puffer/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path('favicon.ico/', RedirectView.as_view(
          url=staticfiles_storage.url('puffer/dist/images/favicon.ico')),
          name='favicon'),
-    path('profile/', views.profile, name='profile'),
     path('player/', views.player, name='player'),
     path('faq/', views.faq, name='faq'),
-    path('participate/', views.participate, name='participate'),
 ]
