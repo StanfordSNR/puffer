4eaec6a92df0aa8b77492e1994b6e5d81ed118c8
Francis Y. Yan <fyy@cs.stanford.edu>
2018-12-27 22:11:27 -0800

    train_ttp.py: read data from InfluxDB

--- src/scripts/train_ttp.py
+++ src/scripts/train_ttp.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import argparse
+import yaml
 import torch
 
+from helpers import connect_to_influxdb
 
-def main():
+
+def train():
     N = 32
     D_in, H, D_out = 5, 4, 3
 
-    # TODO: read data from InfluxDB into x and y
     x = torch.randn(N, D_in)
     y = torch.randn(N, D_out)
 
@@ -32,6 +35,50 @@ def main():
         loss.backward()
         optimizer.step()
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('yaml_settings')
+    parser.add_argument('-s', '--start-days-ago',
+                        type=int, help='>= now() - {}d')
+    parser.add_argument('-e', '--end-days-ago',
+                        type=int, help='<= now() - {}d')
+    # TODO: load a previously trained model to perform daily training
+    # parser.add_argument('-m', '--model', help='model to start training with')
+    args = parser.parse_args()
+
+    with open(args.yaml_settings, 'r') as fh:
+        yaml_settings = yaml.safe_load(fh)
+
+    # construct time clause after 'WHERE'
+    time_clause = None
+    if args.start_days_ago is not None:
+        time_clause = 'time >= now() - {}d'.format(args.start_days_ago)
+    if args.end_days_ago is not None:
+        end_date_clause = 'time <= now() - {}d'.format(args.end_days_ago)
+        if time_clause is None:
+            time_clause = end_date_clause
+        else:
+            time_clause += ' AND ' + end_date_clause
+
+    influx_client = connect_to_influxdb(yaml_settings)
+
+    # perform queries in InfluxDB
+    video_sent_query = 'SELECT * FROM video_sent'
+    if time_clause is not None:
+        video_sent_query += ' WHERE ' + time_clause
+    video_sent_results = influx_client.query(video_sent_query)
+
+    video_acked_query = 'SELECT * FROM video_acked'
+    if time_clause is not None:
+        video_acked_query += ' WHERE ' + time_clause
+    video_acked_results = influx_client.query(video_acked_query)
+
+    # TODO: calculate chunk transmission times by iterating over
+    # video_sent_results and video_acked_results
+
+    # TODO: training
+    # train()
 
 if __name__ == '__main__':
     main()
