f9e216cbb6a79f33f9d0fcee64c9db2338d7f525
Hudson Ayers <hayers@stanford.edu>
2019-01-06 15:13:54 -0800

    Add util code for getting current pid

---
+++ src/util/pid.cc
@@ -0,0 +1,14 @@
+#include "pid.hh"
+#include <exception.hh>
+
+using namespace std;
+
+pid_t pid()
+{
+
+  auto pid = CheckSystemCall("getpid", getpid());
+
+
+  return pid;
+}
+
---
+++ src/util/pid.hh
@@ -0,0 +1,9 @@
+#ifndef PID_HH
+#define PID_HH
+
+#include <sys/types.h>
+#include <unistd.h>
+
+pid_t pid();
+
+#endif /* PID_HH */
