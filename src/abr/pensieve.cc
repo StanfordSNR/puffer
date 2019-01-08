9b9752391af1ba91e5ca9618beb67d86a5425d2e
Hudson Ayers <hayers@stanford.edu>
2019-01-07 17:52:21 -0800

    Match code style to linear_bba

--- src/abr/pensieve.cc
+++ src/abr/pensieve.cc
@@ -1,15 +1,17 @@
 #include "pensieve.hh"
 #include "ws_client.hh"
 #include "pid.hh"
-#include "child_process.hh"
 #include "system_runner.hh"
 #include "serialization.hh"
-#include <random>
+#include "ipc_socket.hh"
+#include "json.hpp"
+#include <stdio.h>
 
+using json = nlohmann::json;
 using namespace std;
 
 Pensieve::Pensieve(const WebSocketClient & client,
-         const string & abr_name, const YAML::Node & abr_config)
+                   const string & abr_name, const YAML::Node & abr_config)
   : ABRAlgo(client, abr_name)
 {
 
@@ -24,6 +26,7 @@ Pensieve::Pensieve(const WebSocketClient & client,
   fs::path ipc_path = fs::current_path() / ipc_file_;
   string pensieve_path;
   string nn_path;
+
   if (abr_config["pensieve_path"] && abr_config["nn_path"]) {
     pensieve_path = abr_config["pensieve_path"].as<string>();
     nn_path = abr_config["nn_path"].as<string>();
@@ -52,9 +55,9 @@ Pensieve::~Pensieve() {
 }
 
 void Pensieve::video_chunk_acked(const VideoFormat &,
-                            const double,
-                            const unsigned int size, //total length in bytes of chunk
-                            const uint64_t trans_time) //ms
+                                 const double,
+                                 const unsigned int size, // bytes
+                                 const uint64_t trans_time) // ms
 {
   const auto & channel = client_.channel();
   const auto & vformats = channel->vformats();
@@ -64,52 +67,50 @@ void Pensieve::video_chunk_acked(const VideoFormat &,
 
   uint64_t next_vts = client_.next_vts().value();
   const auto & data_map = channel->vdata(next_vts);
-  vector<double> next_chunk_sizes; // Store pairs of chunk size, corresponding vf index
+  vector<double> next_chunk_sizes;
+
   for (size_t i = 0; i < vformats_cnt; i++) {
     const auto & vf = vformats[i];
     double chunk_size = (double)get<1>(data_map.at(vf)); // Bytes
     next_chunk_sizes.push_back(chunk_size);
   }
+
   sort(next_chunk_sizes.begin(), next_chunk_sizes.end());
 
   // TODO: Increase trans_time to account for time to send audio chunks?
   json j;
   j["delay"] = trans_time; // ms
-  j["playback_buf"] = client_.video_playback_buf(); // seconds
-  j["rebuf_time"] = client_.cum_rebuffer(); // cum seconds spent rebuffering up till now
+  j["playback_buf"] = client_.video_playback_buf(); // Seconds
+  j["rebuf_time"] = client_.cum_rebuffer(); // Seconds
   j["last_chunk_size"] = (double)size; // Bytes
   j["next_chunk_sizes"] = next_chunk_sizes; //Bytes
   uint16_t json_len = j.dump().length();
-  cout << j.dump() << endl;
 
   connection_.write(put_field(json_len) + j.dump());
 
-  auto read_data = connection_.read_exactly(2); //read 2 byte length
+  auto read_data = connection_.read_exactly(2); // Read 2 byte length
   auto rcvd_json_len = get_uint16( read_data.data() );
   auto read_json = connection_.read_exactly(rcvd_json_len);
   next_br_index_ = json::parse(read_json)["bit_rate"];
-  cout << "Index of next chunk to be sent: " << next_br_index_ << endl;
 }
 
 VideoFormat Pensieve::select_video_format()
 {
-  // Basic design here is that the next_format is set after every video ack,
-  // so any time this is called (which must come after an ack for the previous video chunk) this
-  // value will have been updated.
   const auto & channel = client_.channel();
   const auto & vformats = channel->vformats();
   size_t vformats_cnt = vformats.size();
 
   uint64_t next_vts = client_.next_vts().value();
   const auto & data_map = channel->vdata(next_vts);
-  vector<pair<double, size_t>> next_chunk_sizes; //Store pairs of chunk size, corresponding vf index
+  vector<pair<double, size_t>> next_chunk_sizes; // Store (chunk size, vf index)
+
   for (size_t i = 0; i < vformats_cnt; i++) {
     const auto & vf = vformats[i];
-
-    double chunk_size = (double)get<1>(data_map.at(vf)); //Bytes
+    double chunk_size = (double)get<1>(data_map.at(vf));
     next_chunk_sizes.push_back(make_pair(chunk_size, i));
   }
-  sort(next_chunk_sizes.begin(), next_chunk_sizes.end()); // sorts pairs by first element
-  size_t next_format = get<1>(next_chunk_sizes[next_br_index_]); //Works bc next_chunk_size is sorted
+
+  sort(next_chunk_sizes.begin(), next_chunk_sizes.end()); // sort by 1st element
+  size_t next_format = get<1>(next_chunk_sizes[next_br_index_]);
   return client_.channel()->vformats()[next_format];
 }
