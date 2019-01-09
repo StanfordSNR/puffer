a08ca2165c590a333d71faf9ef5dbb22e8923c58
Francis Y. Yan <fyy@cs.stanford.edu>
2019-01-08 22:50:17 -0800

    Remove unused header files

--- src/abr/pensieve.hh
+++ src/abr/pensieve.hh
@@ -4,14 +4,12 @@
 #include "abr_algo.hh"
 #include "child_process.hh"
 #include "filesystem.hh"
-#include <cstdint>
-#include <cstdlib>
 
 class Pensieve : public ABRAlgo
 {
 public:
-  Pensieve(const WebSocketClient & client, const std::string & abr_name,
-           const YAML::Node & abr_config);
+  Pensieve(const WebSocketClient & client,
+           const std::string & abr_name, const YAML::Node & abr_config);
   ~Pensieve();
 
   void video_chunk_acked(const VideoFormat & format,
