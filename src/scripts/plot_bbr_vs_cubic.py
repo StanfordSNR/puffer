78e64ea8c7e2f1c8171bb7c39d8f16b1a7a54db1
Francis Y. Yan <fyy@cs.stanford.edu>
2019-01-23 21:19:55 -0800

    plot_bbr_vs_cubic.py: plot CDF of rebuffer rate

--- src/scripts/plot_bbr_vs_cubic.py
+++ src/scripts/plot_bbr_vs_cubic.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 from helpers import (
     connect_to_postgres, retrieve_expt_config, ssim_index_to_db)
-from collect_data import collect_video_data, VIDEO_DURATION
+from collect_data import collect_video_data, collect_buffer_data, VIDEO_DURATION
 
 
 # cache of Postgres data: experiment 'id' -> json 'data' of the experiment
@@ -24,11 +24,11 @@ def plot_ssim_cdf(data, args):
     x_max = 25
     num_bins = 100
     for cc in data:
-        counts, bin_edges = np.histogram(data[cc]['ssim'], bins=num_bins,
+        counts, bin_edges = np.histogram(data[cc], bins=num_bins,
                                          range=(x_min, x_max))
 
         x = bin_edges
-        y = np.cumsum(counts) / len(data[cc]['ssim'])
+        y = np.cumsum(counts) / len(data[cc])
         y = np.insert(y, 0, 0)  # prepend 0
 
         ax.plot(x, y, label=cc)
@@ -55,11 +55,11 @@ def plot_ssim_var_cdf(data, args):
     x_max = 4
     num_bins = 100
     for cc in data:
-        counts, bin_edges = np.histogram(data[cc]['ssim_var'], bins=num_bins,
+        counts, bin_edges = np.histogram(data[cc], bins=num_bins,
                                          range=(x_min, x_max))
 
         x = bin_edges
-        y = np.cumsum(counts) / len(data[cc]['ssim_var'])
+        y = np.cumsum(counts) / len(data[cc])
         y = np.insert(y, 0, 0)  # prepend 0
 
         ax.plot(x, y, label=cc)
@@ -80,18 +80,17 @@ def plot_ssim_var_cdf(data, args):
 
 
 def plot_ssim(d, postgres_cursor, args):
-    data = {
-        'cubic': {'ssim': [], 'ssim_var': []},
-        'bbr': {'ssim': [], 'ssim_var': []}
-    }
+    ssim_by_cc = {}
+    ssim_var_by_cc = {}
 
     for session in d:
         expt_id = session[3]
         expt_config = retrieve_expt_config(expt_id, expt_id_cache,
                                            postgres_cursor)
         cc = expt_config['cc']
-        if cc != 'cubic' and cc != 'bbr':
-            continue
+        if cc not in ssim_by_cc:
+            ssim_by_cc[cc] = []
+            ssim_var_by_cc[cc] = []
 
         for video_ts in d[session]:
             dsv = d[session][video_ts]
@@ -102,7 +101,7 @@ def plot_ssim(d, postgres_cursor, args):
                 continue
 
             curr_ssim_db = ssim_index_to_db(curr_ssim_index)
-            data[cc]['ssim'].append(curr_ssim_db)
+            ssim_by_cc[cc].append(curr_ssim_db)
 
             # append SSIM variation
             prev_ts = video_ts - VIDEO_DURATION
@@ -115,13 +114,70 @@ def plot_ssim(d, postgres_cursor, args):
 
             prev_ssim_db = ssim_index_to_db(prev_ssim_index)
             ssim_diff = abs(curr_ssim_db - prev_ssim_db)
-            data[cc]['ssim_var'].append(ssim_diff)
+            ssim_var_by_cc[cc].append(ssim_diff)
 
     # plot CDF of SSIM
-    plot_ssim_cdf(data, args)
+    plot_ssim_cdf(ssim_by_cc, args)
 
-    # plto CDF of SSIM variation
-    plot_ssim_var_cdf(data, args)
+    # plot CDF of SSIM variation
+    plot_ssim_var_cdf(ssim_var_by_cc, args)
+
+
+def plot_rebuf_rate_cdf(data, args):
+    fig, ax = plt.subplots()
+
+    x_min = None
+    x_max = None
+    for cc in data:
+        data_min = np.min(data[cc])
+        if x_min is None or data_min < x_min:
+            x_min = data_min
+
+        data_max = np.max(data[cc])
+        if x_max is None or data_max > x_max:
+            x_max = data_max
+
+    num_bins = 100
+    for cc in data:
+        counts, bin_edges = np.histogram(data[cc], bins=num_bins,
+                                         range=(x_min, x_max))
+
+        x = bin_edges
+        y = np.cumsum(counts) / len(data[cc])
+        y = np.insert(y, 0, 0)  # prepend 0
+
+        ax.plot(x, y, label=cc)
+
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(0, 1)
+    ax.legend()
+    ax.grid()
+
+    title = '[{}, {}] (UTC)'.format(args.time_start, args.time_end)
+    ax.set_title(title)
+    ax.set_xlabel('Rebuffer rate (%)')
+    ax.set_ylabel('CDF')
+
+    figname = 'bbr_cubic_rebuf_rate_cdf.png'
+    fig.savefig(figname, dpi=150, bbox_inches='tight')
+    sys.stderr.write('Saved plot to {}\n'.format(figname))
+
+
+def plot_rebuf_rate(d, postgres_cursor, args):
+    rebuf_rate_by_cc = {}
+
+    for session in d:
+        expt_id = session[3]
+        expt_config = retrieve_expt_config(expt_id, expt_id_cache,
+                                           postgres_cursor)
+        cc = expt_config['cc']
+        if cc not in rebuf_rate_by_cc:
+            rebuf_rate_by_cc[cc] = []
+
+        rebuf_rate_by_cc[cc].append(100 * d[session]['rebuf'] / d[session]['play'])
+
+    # plot CDF of rebuffer rate
+    plot_rebuf_rate_cdf(rebuf_rate_by_cc, args)
 
 
 def main():
@@ -140,12 +196,18 @@ def main():
     video_data = collect_video_data(yaml_settings_path,
                                     args.time_start, args.time_end, None)
 
+    buffer_data = collect_buffer_data(yaml_settings_path,
+                                      args.time_start, args.time_end)
+
     # create a client connected to Postgres
     postgres_client = connect_to_postgres(yaml_settings)
     postgres_cursor = postgres_client.cursor()
 
-    # plot SSIM and SSIM variation
+    # plot SSIM and its variation
     plot_ssim(video_data, postgres_cursor, args)
+
+    # plot rebuffer rate
+    plot_rebuf_rate(buffer_data, postgres_cursor, args)
 
     postgres_cursor.close()
 
