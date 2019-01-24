4a38f12025de6548590572c67f11bbd99d0cf4a5
Hudson Ayers <hayers@stanford.edu>
2019-01-24 22:34:59 +0000

    Improve behavior of offline test for ctrl-c

--- src/media-server/offline_test.py
+++ src/media-server/offline_test.py
@@ -11,34 +11,41 @@ def run_offline_media_servers():
 
 
 def start_maimahi_clients(num_clients):
-    trace_dir = "/home/ubuntu/fcc_mahimahi_traces"
-    files = os.listdir(trace_dir)
-    for filename in files:
-        mahimahi_cmd = 'mm-delay 40 mm-link 12mbps ' + trace_dir + '/' + \
-                        filename
-        plist = []
-        base_port = 9362
-        for i in range(0, num_clients + 1):
-            port = base_port + i
-            chrome_cmd = 'chromium-browser --headless --disable-gpu --remote-debugging-port=9222 ' + \
-                         'http://$MAHIMAHI_BASE:8080/player/?wsport=' + \
-                         str(port) + ' --user-data-dir=./' + str(port) + \
-                         '.profile'
-            chrome_cmd_b = chrome_cmd.encode('utf-8')
-            p = subprocess.Popen(mahimahi_cmd, shell=True,
-                                 stdin=subprocess.PIPE, preexec_fn=os.setsid)
-            p.stdin.write(chrome_cmd_b)
-            p.stdin.flush()
-            p.stdin.close()
-            plist.append(p)
+    plist = []
+    try:
+        trace_dir = "/home/ubuntu/fcc_mahimahi_traces"
+        files = os.listdir(trace_dir)
+        for filename in files:
+            mahimahi_cmd = 'mm-delay 40 mm-link 12mbps ' + trace_dir + '/' + \
+                            filename
+            base_port = 9361
+            plist = []
+            for i in range(1, num_clients + 1):
+                port = base_port + i
+                chrome_cmd = 'chromium-browser --headless --disable-gpu --remote-debugging-port=9222 ' + \
+                             'http://$MAHIMAHI_BASE:8080/player/?wsport=' + \
+                             str(port) + ' --user-data-dir=./' + str(port) + \
+                             '.profile'
+                chrome_cmd_b = chrome_cmd.encode('utf-8')
+                p = subprocess.Popen(mahimahi_cmd, shell=True,
+                                     stdin=subprocess.PIPE, preexec_fn=os.setsid)
+                p.stdin.write(chrome_cmd_b)
+                p.stdin.flush()
+                p.stdin.close()
+                plist.append(p)
 
-        time.sleep(60*10)
+            time.sleep(60*10)
+            for p in plist:
+                os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+
+            subprocess.check_call("killall chromium-browser", shell=True)
+            subprocess.check_call("rm -rf ./*.profile", shell=True,
+                                  executable='/bin/bash')
+    except:
+        pass
+    finally:
         for p in plist:
             os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-
-        subprocess.check_call("killall chromium-browser", shell=True)
-        subprocess.check_call("rm -rf ./*.profile", shell=True,
-                              executable='/bin/bash')
 
 
 def main():
