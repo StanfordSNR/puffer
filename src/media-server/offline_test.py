e78e98851fbbcc2693e850353abd2c917e2bc4da
Hudson Ayers <hayers@stanford.edu>
2019-01-25 04:39:38 +0000

    cleanup  and fixes

--- src/media-server/offline_test.py
+++ src/media-server/offline_test.py
@@ -18,7 +18,7 @@ def start_maimahi_clients(num_clients):
         files = os.listdir(trace_dir)
         for filename in files:
             # mahimahi_cmd = 'mm-delay 40 mm-link 12mbps ' + trace_dir + '/' + \
-                            filename
+            #                filename
             base_port = 9361
             plist = []
             for i in range(1, num_clients + 1):
@@ -27,12 +27,10 @@ def start_maimahi_clients(num_clients):
                 #             'http://$MAHIMAHI_BASE:8080/player/?wsport=' + \
                 #             str(port) + ' --user-data-dir=./' + str(port) + \
                 #             '.profile'
-                mahimahi_chrome_cmd = "mm-link 12mbps.trace {}/{} --meter-downlink -- \
-                sh -c 'chromium-browser --headless --disable-gpu --remote-debugging-port=9222 \
-                http://$MAHIMAHI_BASE/player/?wsport={} \
-                --user-data-dir=./{}.profile'".format(trace_dir, filename, port, port)
+                mahimahi_chrome_cmd = "mm-delay 40 mm-link 12mbps {}/{} --meter-downlink -- sh -c 'chromium-browser --headless --disable-gpu --remote-debugging-port=9222 http://$MAHIMAHI_BASE:8080/player/?wsport={} --user-data-dir=./{}.profile'".format(trace_dir, filename, port, port)
+                print(mahimahi_chrome_cmd)
                 chrome_cmd_b = mahimahi_chrome_cmd.encode('utf-8')
-                p = subprocess.Popen(mahimahi_cmd, shell=True,
+                p = subprocess.Popen(mahimahi_chrome_cmd, shell=True,
                                      preexec_fn=os.setsid)
                 plist.append(p)
 
@@ -43,17 +41,20 @@ def start_maimahi_clients(num_clients):
             subprocess.check_call("killall chromium-browser", shell=True)
             subprocess.check_call("rm -rf ./*.profile", shell=True,
                                   executable='/bin/bash')
-    except:
+    except Exception as e:
+        print("exception: " + str(e))
         pass
     finally:
         for p in plist:
             os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+            subprocess.check_call("rm -rf ./*.profile", shell=True,
+                                  executable='/bin/bash')
 
 
 def main():
     subprocess.check_call('sudo sysctl -w net.ipv4.ip_forward=1', shell=True)
     # run_media_servers()
-    start_maimahi_clients(2)
+    start_maimahi_clients(4)
 
 
 if __name__ == '__main__':
