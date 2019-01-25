8be3a95d486acb6e429fa2f2b845af1b22359f80
Francis Y. Yan <fyy@cs.stanford.edu>
2019-01-24 17:40:37 -0800

    plot_bbr_vs_cubic.py: use helper functions in collect_data

--- src/scripts/plot_bbr_vs_cubic.py
+++ src/scripts/plot_bbr_vs_cubic.py
@@ -9,12 +9,10 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from helpers import (
-    connect_to_postgres, retrieve_expt_config, ssim_index_to_db)
-from collect_data import collect_video_data, collect_buffer_data, VIDEO_DURATION
-
-
-# cache of Postgres data: experiment 'id' -> json 'data' of the experiment
-expt_id_cache = {}
+    connect_to_postgres, connect_to_influxdb, retrieve_expt_config,
+    ssim_index_to_db, query_measurement)
+from collect_data import (
+    video_data_by_session, buffer_data_by_session, VIDEO_DURATION)
 
 
 def plot_ssim_cdf(data, args):
@@ -79,12 +77,12 @@ def plot_ssim_var_cdf(data, args):
     sys.stderr.write('Saved plot to {}\n'.format(figname))
 
 
-def plot_ssim(d, postgres_cursor, args):
+def plot_ssim(d, expt_id_cache, postgres_cursor, args):
     ssim_by_cc = {}
     ssim_var_by_cc = {}
 
     for session in d:
-        expt_id = session[3]
+        expt_id = int(session[-1])
         expt_config = retrieve_expt_config(expt_id, expt_id_cache,
                                            postgres_cursor)
         cc = expt_config['cc']
@@ -123,27 +121,30 @@ def plot_ssim(d, postgres_cursor, args):
     plot_ssim_var_cdf(ssim_var_by_cc, args)
 
 
-def plot_rebuf_rate_cdf(data, args):
+def plot_rebuf_time_cdf(d, args):
     fig, ax = plt.subplots()
 
     x_min = None
     x_max = None
-    for cc in data:
-        data_min = np.min(data[cc])
+    for cc in d:
+        data_min = np.min(d[cc])
         if x_min is None or data_min < x_min:
             x_min = data_min
 
-        data_max = np.max(data[cc])
+        data_max = np.max(d[cc])
         if x_max is None or data_max > x_max:
             x_max = data_max
 
+    if x_min == x_max:
+        sys.exit('Error: not enough data to plot rebuffer rate')
+
     num_bins = 100
-    for cc in data:
-        counts, bin_edges = np.histogram(data[cc], bins=num_bins,
+    for cc in d:
+        counts, bin_edges = np.histogram(d[cc], bins=num_bins,
                                          range=(x_min, x_max))
 
         x = bin_edges
-        y = np.cumsum(counts) / len(data[cc])
+        y = np.cumsum(counts) / len(d[cc])
         y = np.insert(y, 0, 0)  # prepend 0
 
         ax.plot(x, y, label=cc)
@@ -155,29 +156,29 @@ def plot_rebuf_rate_cdf(data, args):
 
     title = '[{}, {}] (UTC)'.format(args.time_start, args.time_end)
     ax.set_title(title)
-    ax.set_xlabel('Rebuffer rate (%)')
+    ax.set_xlabel('Rebuffer time (min)')
     ax.set_ylabel('CDF')
 
-    figname = 'bbr_cubic_rebuf_rate_cdf.png'
+    figname = 'bbr_cubic_rebuf_time_cdf.png'
     fig.savefig(figname, dpi=150, bbox_inches='tight')
     sys.stderr.write('Saved plot to {}\n'.format(figname))
 
 
-def plot_rebuf_rate(d, postgres_cursor, args):
-    rebuf_rate_by_cc = {}
+def plot_rebuf_time(d, expt_id_cache, postgres_cursor, args):
+    rebuf_time_by_cc = {}
 
     for session in d:
-        expt_id = session[3]
+        expt_id = int(session[-1])
         expt_config = retrieve_expt_config(expt_id, expt_id_cache,
                                            postgres_cursor)
         cc = expt_config['cc']
-        if cc not in rebuf_rate_by_cc:
-            rebuf_rate_by_cc[cc] = []
+        if cc not in rebuf_time_by_cc:
+            rebuf_time_by_cc[cc] = []
 
-        rebuf_rate_by_cc[cc].append(100 * d[session]['rebuf'] / d[session]['play'])
+        if d[session]['rebuf'] > 0:
+            rebuf_time_by_cc[cc].append(d[session]['rebuf'] / 60.0)
 
-    # plot CDF of rebuffer rate
-    plot_rebuf_rate_cdf(rebuf_rate_by_cc, args)
+    plot_rebuf_time_cdf(rebuf_time_by_cc, args)
 
 
 def main():
@@ -193,21 +194,34 @@ def main():
     with open(yaml_settings_path, 'r') as fh:
         yaml_settings = yaml.safe_load(fh)
 
-    video_data = collect_video_data(yaml_settings_path,
-                                    args.time_start, args.time_end, None)
+    # create an InfluxDB client and perform queries
+    influx_client = connect_to_influxdb(yaml_settings)
 
-    buffer_data = collect_buffer_data(yaml_settings_path,
-                                      args.time_start, args.time_end)
+    # query data from video_sent and video_acked
+    video_sent_results = query_measurement(influx_client, 'video_sent',
+                                           args.time_start, args.time_end)
+    video_acked_results = query_measurement(influx_client, 'video_acked',
+                                            args.time_start, args.time_end)
+    video_data = video_data_by_session(video_sent_results, video_acked_results)
+
+    # query data from client_buffer
+    client_buffer_results = query_measurement(influx_client, 'client_buffer',
+                                              args.time_start, args.time_end)
+
+    buffer_data = buffer_data_by_session(client_buffer_results)
+
+    # cache of Postgres data: experiment 'id' -> json 'data' of the experiment
+    expt_id_cache = {}
 
     # create a client connected to Postgres
     postgres_client = connect_to_postgres(yaml_settings)
     postgres_cursor = postgres_client.cursor()
 
     # plot SSIM and its variation
-    plot_ssim(video_data, postgres_cursor, args)
+    plot_ssim(video_data, expt_id_cache, postgres_cursor, args)
 
-    # plot rebuffer rate
-    plot_rebuf_rate(buffer_data, postgres_cursor, args)
+    # plot rebuffer times
+    plot_rebuf_time(buffer_data, expt_id_cache, postgres_cursor, args)
 
     postgres_cursor.close()
 
