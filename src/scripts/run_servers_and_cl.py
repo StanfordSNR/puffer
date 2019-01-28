2fc8d437b9e8578901c598026920621d576a37d7
Francis Y. Yan <fyy@cs.stanford.edu>
2019-01-28 07:54:13 -0800

    Fix run_servers_and_cl.py: check if abr_name exists first

--- src/scripts/run_servers_and_cl.py
+++ src/scripts/run_servers_and_cl.py
@@ -22,9 +22,11 @@ def run_ttp(ttp_path, yaml_settings_path):
 
     for expt in yaml_settings['experiments']:
         fingerprint = expt['fingerprint']
-        abr_name = fingerprint['abr_name']
-        if abr_name != 'puffer_ttp_cl':
-            continue
+
+        if 'abr_name' in fingerprint:
+            abr_name = fingerprint['abr_name']
+            if abr_name != 'puffer_ttp_cl':
+                continue
 
         cc = fingerprint['cc']
 
