0c15e9cba183a9b292ee2b8c679c8f05162ee170
Chenzhi Zhu <zhucz16@mails.tsinghua.edu.cn>
2019-01-30 19:12:12 +0000

    Add figure for comparing bbr vs cubic

--- src/scripts/pkl_ssim_rebuffer_by_cc.py
+++ src/scripts/pkl_ssim_rebuffer_by_cc.py
@@ -10,12 +10,12 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from helpers import (
-    ssim_index_to_db, get_abr_cc, pretty_names, pretty_colors)
+    ssim_index_to_db, get_abr_cc, pretty_names, pretty_colors, abr_order)
 from collect_data import VIDEO_DURATION
 
 
-def collect_ssim(d, expt_id_cache, args):
-    ssim = {}
+def collect_video_data(d, expt_id_cache, args):
+    x = {}
 
     for session in d:
         expt_id = session[-1]
@@ -26,35 +26,32 @@ def collect_ssim(d, expt_id_cache, args):
         else:
             abr_cc = tuple(expt_id.split('+'))
 
-        if abr_cc not in ssim:
-            ssim[abr_cc] = []
+        if abr_cc not in x:
+            x[abr_cc] = []
 
         for video_ts in d[session]:
-            dsv = d[session][video_ts]
-            curr_ssim_index = dsv['ssim_index']
-
             # append SSIM index
-            ssim[abr_cc].append(curr_ssim_index)
+            x[abr_cc].append(d[session][video_ts]['ssim_index'])
 
-    ssim_mean = {}
-    ssim_sem = {}
+    y = {}
 
-    for abr_cc in ssim:
-        ssim_index_mean = np.mean(ssim[abr_cc])
-        sem = np.std(ssim[abr_cc]) / np.sqrt(len(ssim[abr_cc]))
+    for abr_cc in x:
+        y[abr_cc] = {'ssim_mean': ssim_index_to_db(np.mean(x[abr_cc]))}
+        #ssim_index_mean = np.mean(x[abr_cc])
+        #sem = np.std(ssim[abr_cc]) / np.sqrt(len(ssim[abr_cc]))
 
-        ssim_db_lower = ssim_index_to_db(ssim_index_mean - sem)
-        ssim_db_upper = ssim_index_to_db(ssim_index_mean + sem)
+#        ssim_db_lower = ssim_index_to_db(ssim_index_mean - sem)
+#        ssim_db_upper = ssim_index_to_db(ssim_index_mean + sem)
 
-        ssim_db_mean = ssim_index_to_db(ssim_index_mean)
-        ssim_mean[abr_cc] = ssim_db_mean
-        ssim_sem[abr_cc] = (ssim_db_mean - ssim_db_lower,
-                            ssim_db_upper - ssim_db_mean)
+ #       ssim_db_mean = ssim_index_to_db(ssim_index_mean)
+ #       ssim_mean[abr_cc] = ssim_db_mean
+ #       ssim_sem[abr_cc] = (ssim_db_mean - ssim_db_lower,
+ #                           ssim_db_upper - ssim_db_mean)
 
-    return ssim_mean, ssim_sem
+    return y
 
 
-def collect_rebuffer(d, expt_id_cache, args):
+def collect_buffer_data(d, expt_id_cache, args):
     total_play = {}
     total_rebuf = {}
 
@@ -74,12 +71,12 @@ def collect_rebuffer(d, expt_id_cache, args):
         total_play[abr_cc] += d[session]['play']
         total_rebuf[abr_cc] += d[session]['rebuf']
 
-    rebuffer_rate = {}
+    y = {}
 
     for abr_cc in total_play:
-        rebuffer_rate[abr_cc] = total_rebuf[abr_cc] / total_play[abr_cc]
+        y[abr_cc] = {'rebuf_mean': total_rebuf[abr_cc] / total_play[abr_cc]}
 
-    return rebuffer_rate
+    return y
 
 
 def filt_fast(video_data, buffer_data, min_tput):
@@ -110,7 +107,10 @@ def filt_fast(video_data, buffer_data, min_tput):
     print('Number of left buffer_data sessions:', len(buffer_data))
 
 
-def plot_ssim_mean_vs_rebuf_rate(ssim_mean, ssim_sem, rebuffer_rate, args):
+def plot_ssim_mean_vs_rebuf_rate(x_mean, y_mean, args):
+
+    # x for rebuf_rate
+    # y for ssim
 
     if args.filt_fast:
         xmin = 0
@@ -133,15 +133,13 @@ def plot_ssim_mean_vs_rebuf_rate(ssim_mean, ssim_sem, rebuffer_rate, args):
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
 
-        for abr_cc in ssim_mean:
+        for abr_cc in x_mean:
             if abr_cc[1] != cc:
                 continue
             abr = abr_cc[0]
 
-            x = rebuffer_rate[abr_cc] * 100  # %
-            y = ssim_mean[abr_cc]  # dB
-            sem = ssim_sem[abr_cc]
-            print(abr_cc, x, y, sem)
+            x = x_mean[abr_cc] * 100  # %
+            y = y_mean[abr_cc]  # dB
 
             ax.scatter(x, y, color=pretty_colors[abr])
             # error bars are too small
@@ -157,14 +155,114 @@ def plot_ssim_mean_vs_rebuf_rate(ssim_mean, ssim_sem, rebuffer_rate, args):
             fig_name = '{}_ssim_rebuffer_slow'.format(cc)
         else:
             fig_name = '{}_ssim_rebuffer'.format(cc)
-        fig.savefig(fig_name + '.png')
-        sys.stderr.write('Saved plot to {}\n'.format(fig_name + '.png'))
+        fig.savefig(fig_name + '.svg')
+        sys.stderr.write('Saved plot to {}\n'.format(fig_name + '.svg'))
+
+
+def plot_compare_dots(x_mean, y_mean, args):
+
+    # x for rebuf_rate
+    # y for ssim
+
+    if args.filt_fast:
+        xmin = 0
+        xmax = 6
+        ymin = 12.5
+        ymax = 16
+    else:
+        xmin = 0
+        xmax = 0.85
+        ymin = 15.1
+        ymax = 17.1
+
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel('Time spent stalled (%)')
+    ax.set_ylabel('Average SSIM (dB)')
+
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    for abr in abr_order:
+        if not ((abr, 'bbr') in x_mean and (abr, 'cubic') in x_mean and
+           (abr, 'bbr') in y_mean and (abr, 'cubic') in y_mean):
+                continue
+
+        abr_cc = (abr, 'cubic')
+        abr_cc_ = (abr, 'bbr')
+
+        x = x_mean[abr_cc] * 100  # %
+        y = y_mean[abr_cc]  # dB
+        x_ = x_mean[abr_cc_] * 100  # %
+        y_ = y_mean[abr_cc_]  # dB
+
+        ax.arrow(x, y, x_ - x, y_ - y, color=pretty_colors[abr],
+                 length_includes_head=True, head_starts_at_zero=True,
+                 width=0.01 * (xmax - xmin))
+        #ax.plot([x, x_], [y, y_], color=pretty_colors[abr], linestyle='-')
+        ax.annotate(pretty_names[abr], (x, y))
+        #ax.annotate(pretty_names[abr], xy=(x_, y_), xytext=(x, y),
+                    #arrowprops=dict(arrowstyle="->"), color=pretty_colors[abr])
+
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+
+    ax.invert_xaxis()
+
+    if args.filt_fast:
+        fig_name = 'compare_cc_slow'
+    else:
+        fig_name = 'compare_cc'
+    fig.savefig(fig_name + '.svg')
+    sys.stderr.write('Saved plot to {}\n'.format(fig_name + '.svg'))
+
+
+
+def combine_by_cc(d1, d2):
+    x = {}
+    for abr in d1:
+        x[abr] = d1[abr]
+        x[abr].update(d2[abr])
+
+    return x
+
+
+def pick_by_cc(d, tag):
+    return {abr:d[abr][tag] for abr in d}
+
+
+def print_d(d, output):
+    fp = open(output, 'w')
+
+    for abr in d:
+        fp.write(str(list(d[abr].keys())))
+        break
+    for cc in ['bbr', 'cubic']:
+        fp.write(pretty_names[cc] + '\n')
+        for abr in abr_order:
+            if (abr, cc) not in d:
+                continue
+            fp.write(abr + ':')
+            ds = d[(abr, cc)]
+            for k in ds:
+                fp.write(' ' + str(ds[k]) + ',')
+            fp.write('\n')
+
+        fp.write('\n')
+
+    sys.stderr.write('Print to {}\n'.format(output))
 
 
 def plot(expt_id_cache, args):
-    if args.pre_dp != None and os.path.isfile(args.pre_dp):
-        with open(args.pre_dp, 'rb') as fp:
-            ssim_mean, ssim_sem, rebuffer_rate = pickle.load(fp)
+    if args.filt_fast:
+        pre_dp = args.pre_dp + '_slow.pickle'
+    else:
+        pre_dp = args.pre_dp + '.pickle'
+
+    if pre_dp != None and os.path.isfile(pre_dp):
+        with open(pre_dp, 'rb') as fp:
+            d = pickle.load(fp)
     else:
         with open(args.video_data_pickle, 'rb') as fh:
             video_data = pickle.load(fh)
@@ -177,30 +275,38 @@ def plot(expt_id_cache, args):
         if args.filt_fast:
             filt_fast(video_data, buffer_data, 6)
 
-        ssim_mean, ssim_sem = collect_ssim(video_data, expt_id_cache, args)
-        rebuffer_rate = collect_rebuffer(buffer_data, expt_id_cache, args)
+        dv = collect_video_data(video_data, expt_id_cache, args)
+        db = collect_buffer_data(buffer_data, expt_id_cache, args)
+        d = combine_by_cc(dv, db)
 
-        with open(args.pre_dp, 'wb') as fp:
-            pickle.dump((ssim_mean, ssim_sem, rebuffer_rate), fp)
+        with open(pre_dp, 'wb') as fp:
+            pickle.dump(d, fp)
 
-    plot_ssim_mean_vs_rebuf_rate(ssim_mean, ssim_sem, rebuffer_rate, args)
+    print_d(d, 'ssim_buffer.txt')
+
+    if args.task == 'dots':
+        plot_ssim_mean_vs_rebuf_rate(pick_by_cc(d, 'rebuf_mean'),
+                pick_by_cc(d, 'ssim_mean'), args)
+    elif args.task == 'compare':
+        plot_compare_dots(pick_by_cc(d, 'rebuf_mean'),
+                       pick_by_cc(d, 'ssim_mean'), args)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--video-data-pickle', required=True)
     parser.add_argument('-b', '--buffer-data-pickle', required=True)
-    parser.add_argument('-e', '--expt-id-cache')
+    parser.add_argument('-e', '--expt-id-cache',
+                        default='expt_id_cache.pickle')
     parser.add_argument('--emu', action='store_true')
     parser.add_argument('--filt-fast', action='store_true',
                         help='filt ithe session with average tput faster than\
                               6 Mpbs')
-    parser.add_argument('--pre-dp', default='ssim_rebuffer.pickle')
+    parser.add_argument('--pre-dp', default='ssim_rebuffer')
+    parser.add_argument('-t', '--task', default='dots')
     args = parser.parse_args()
 
     if not args.emu:
-        assert(args.expt_id_cache)
-
         with open(args.expt_id_cache, 'rb') as fh:
             expt_id_cache = pickle.load(fh)
 
