7ad33b2a3d1e5a9d5597e017425399f427be3fe0
Francis Y. Yan <fyy@cs.stanford.edu>
2019-06-23 20:50:46 -0700

    Add timestamp_ns, timestamp_us and timestamp_s

--- src/util/timestamp.cc
+++ src/util/timestamp.cc
@@ -1,18 +1,29 @@
 #include "timestamp.hh"
-
+#include <ctime>
 #include "exception.hh"
 
-/* nanoseconds per millisecond */
-static const uint64_t MILLION = 1000000;
-
-/* nanoseconds per second */
-static const uint64_t BILLION = 1000 * MILLION;
-
-uint64_t timestamp_ms()
+uint64_t timestamp_ns()
 {
   timespec ts;
   CheckSystemCall("clock_gettime", clock_gettime(CLOCK_REALTIME, &ts));
 
-  const uint64_t nanos = ts.tv_sec * BILLION + ts.tv_nsec;
-  return nanos / MILLION;
+  return ts.tv_sec * BILLION + ts.tv_nsec;
+}
+
+uint64_t timestamp_us()
+{
+  return timestamp_ns() / 1000;
+}
+
+uint64_t timestamp_ms()
+{
+  return timestamp_ns() / MILLION;
+}
+
+uint64_t timestamp_s()
+{
+  timespec ts;
+  CheckSystemCall("clock_gettime", clock_gettime(CLOCK_REALTIME, &ts));
+
+  return ts.tv_sec;
 }
--- src/util/timestamp.hh
+++ src/util/timestamp.hh
@@ -1,10 +1,21 @@
 #ifndef TIMESTAMP_HH
 #define TIMESTAMP_HH
 
-#include <ctime>
 #include <cstdint>
+
+const uint64_t MILLION = 1000 * 1000;
+const uint64_t BILLION = 1000 * 1000 * 1000;
+
+/* nanoseconds since epoch */
+uint64_t timestamp_ns();
+
+/* microseconds since epoch */
+uint64_t timestamp_us();
 
 /* milliseconds since epoch */
 uint64_t timestamp_ms();
+
+/* seconds since epoch */
+uint64_t timestamp_s();
 
 #endif /* TIMESTAMP_HH */
