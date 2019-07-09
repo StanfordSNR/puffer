806217efb771ac70533633ad117c0cbf6402cc9f
Francis Y. Yan <fyy@cs.stanford.edu>
2019-07-08 18:07:37 -0700

    log_reporter.cc: increment 1 ns for duplicate timestamp

--- src/monitoring/log_reporter.cc
+++ src/monitoring/log_reporter.cc
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <string>
 #include <fstream>
 
@@ -16,6 +17,7 @@
 #include "exception.hh"
 #include "formatter.hh"
 #include "influxdb_client.hh"
+#include "timestamp.hh"
 
 using namespace std;
 using namespace PollerShortNames;
@@ -30,8 +32,58 @@ void print_usage(const string & program_name)
   << endl;
 }
 
-int tail_loop(const YAML::Node & config, const string & log_path)
+tuple<string, string> enforce_unique(const string & line,
+                                     deque<uint64_t> & unique_ns)
 {
+  auto last_space = line.rfind(' ');
+  string before_last_space = line.substr(0, last_space);
+  string orig_ts_str = line.substr(last_space + 1);
+
+  const uint64_t orig_ts_ns = stoll(orig_ts_str) * MILLION;  /* ms -> ns */
+
+  /* pop if ts in ms < orig_new_ts in ms */
+  while (not unique_ns.empty()) {
+    const auto ts_ns = unique_ns.front();
+    if (ts_ns / MILLION < orig_ts_ns / MILLION) {
+      unique_ns.pop_front();
+    } else {
+      break;
+    }
+  }
+
+  /* increment the last ts in unique_ns by 1 ns */
+  uint64_t new_ts_ns = orig_ts_ns;
+  if (not unique_ns.empty()) {
+    const auto ts_ns = unique_ns.back();
+
+    if ((ts_ns + 1) / MILLION != new_ts_ns / MILLION) {
+      throw runtime_error("enforce_unique: " + to_string(ts_ns) +
+                          ", " + to_string(new_ts_ns));
+    }
+
+    new_ts_ns = ts_ns + 1;
+  }
+  unique_ns.push_back(new_ts_ns);
+
+  if (new_ts_ns == orig_ts_ns) {
+    /* no duplicate timestamp exists */
+    return make_tuple(line, "ms");
+  } else {
+    /* duplicate timestamp exists; post the incremented timestamp */
+    string new_line = before_last_space + " " + to_string(new_ts_ns);
+    return make_tuple(new_line, "ns");
+  }
+}
+
+int tail_loop(const YAML::Node & config, const string & log_path,
+              const string & measurement)
+{
+  /* enforce uniqueness for the crucial measurements below */
+  bool crucial_measurements = (measurement == "client_buffer" ||
+                               measurement == "video_acked" ||
+                               measurement == "video_sent");
+  deque<uint64_t> unique_ns;
+
   Poller poller;
   Inotify inotify(poller);
 
@@ -52,7 +104,8 @@ int tail_loop(const YAML::Node & config, const string & log_path)
     fd.seek(0, SEEK_END);
 
     int wd = inotify.add_watch(log_path, IN_MODIFY | IN_CLOSE_WRITE,
-      [&log_rotated, &buf, &fd, &influxdb_client]
+      [&log_rotated, &buf, &fd, &influxdb_client,
+       crucial_measurements, &unique_ns]
       (const inotify_event & event, const string &) {
         if (event.mask & IN_MODIFY) {
           string new_content = fd.read();
@@ -70,16 +123,24 @@ int tail_loop(const YAML::Node & config, const string & log_path)
             const string & line = buf.substr(0, pos);
             vector<string> values = split(line, ",");
 
-            /* aggregate multiple points and separate by new lines */
-            if (not payload.empty()) {
-              payload += "\n";
+            /* enforce data uniqueness for crucial measurements */
+            if (crucial_measurements) {
+              auto [new_line, precision] = enforce_unique(
+                  formatter.format(values), unique_ns);
+              if (precision == "ms") {
+                payload += new_line + "\n";
+              } else {
+                /* post data points with other time precisions immediately */
+                influxdb_client.post(new_line, precision);
+              }
+            } else {
+              payload += formatter.format(values) + "\n";
             }
-            payload += formatter.format(values);
 
             buf = buf.substr(pos + 1);
           }
 
-          /* post aggregated lines */
+          /* post aggregated lines (with time precision 'ms') */
           influxdb_client.post(payload);
         } else if (event.mask & IN_CLOSE_WRITE) {
           /* old log was closed; open and watch new log in next loop */
@@ -133,6 +194,9 @@ int main(int argc, char * argv[])
   getline(format_ifstream, format_string);
   formatter.parse(format_string);
 
+  auto comma_pos = format_string.find(',');
+  string measurement = format_string.substr(0, comma_pos);
+
   /* read new lines from logs and post to InfluxDB */
-  return tail_loop(config, log_path);
+  return tail_loop(config, log_path, measurement);
 }
