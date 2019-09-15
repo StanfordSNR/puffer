c89031c2cb6ebe2ebe26c50660b26836bb4cf636
Hudson Ayers <hayers@stanford.edu>
2019-09-15 19:35:01 +0000

    emulation settings for nsdi bbr

--- src/media-server/offline_test.py
+++ src/media-server/offline_test.py
@@ -58,7 +58,7 @@ def start_maimahi_clients(num_clients):
 def main():
     subprocess.check_call('sudo sysctl -w net.ipv4.ip_forward=1', shell=True)
     #run_offline_media_servers()
-    start_maimahi_clients(5)
+    start_maimahi_clients(10)
 
 
 if __name__ == '__main__':
