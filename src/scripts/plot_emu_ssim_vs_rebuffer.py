c462f0ab5fc7222f692f7f91e108980e55581954
Francis Y. Yan <fyy@cs.stanford.edu>
2019-09-18 13:06:30 -0700

    plot_emu_ssim_vs_rebuffer.py: change to output weighted average of 'average SSIM per session', weighted by watch time

--- src/scripts/plot_emu_ssim_vs_rebuffer.py
+++ src/scripts/plot_emu_ssim_vs_rebuffer.py
@@ -1,40 +1,91 @@
 #!/usr/bin/env python3
 
 import os
+from os import path
 import sys
 import argparse
 import yaml
-import numpy as np
+import json
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from helpers import (
-    connect_to_influxdb, ssim_index_to_db, get_ssim_index,
-    pretty_colors, pretty_names)
+    connect_to_influxdb, ssim_index_to_db, pretty_colors, pretty_names)
 from collect_data import video_data_by_session, buffer_data_by_session
 
 
 args = None
+influx_client = None
 
 
-def plot_ssim_rebuffer(ssim, rebuffer):
+def get_video_data():
+    video_sent_results = influx_client.query('SELECT * FROM video_sent')
+    video_acked_results = influx_client.query('SELECT * FROM video_acked')
+    video_data = video_data_by_session(video_sent_results, video_acked_results)
+
+    for session in video_data:
+        ssim_index_ctr = [0.0, 0]  # sum, count
+        for ts in video_data[session]:
+            a = video_data[session][ts]  # short name
+            ssim_index_ctr[0] += a['ssim_index']
+            ssim_index_ctr[1] += 1
+        video_data[session] = ssim_index_ctr[0] / ssim_index_ctr[1]
+
+    return video_data
+
+
+def get_buffer_data():
+    client_buffer_results = influx_client.query('SELECT * FROM client_buffer')
+    buffer_data = buffer_data_by_session(client_buffer_results)
+
+    return buffer_data
+
+
+def get_data():
+    video_data = get_video_data()
+    buffer_data = get_buffer_data()
+
+    # merge common sessions
+    common_sessions = set(video_data) & set(buffer_data)
+
+    data = {}
+    for session in sorted(common_sessions, key=lambda s: int(s[1])):
+        expt_id = session[-1]
+
+        abr, cc = tuple(expt_id.split('+'))
+        if cc != 'bbr':
+            continue
+
+        if abr not in data:
+            # weighted SSIM (index) sum, total stall, total watch time
+            data[abr] = [0.0, 0.0, 0.0]
+
+        # short names
+        vs = video_data[session]
+        bs = buffer_data[session]
+
+        data[abr][0] += vs * bs['play']
+        data[abr][1] += bs['rebuf']
+        data[abr][2] += bs['play']
+
+    for abr in data:
+        avg_ssim_db = ssim_index_to_db(data[abr][0] / data[abr][2])
+        stall_ratio = data[abr][1] / data[abr][2]
+
+        data[abr] = [stall_ratio, avg_ssim_db]
+
+    return data
+
+
+def plot_ssim_rebuffer(data):
     fig, ax = plt.subplots()
-    ax.set_xlabel('Time spent stalled (%)')
-    ax.set_ylabel('Average SSIM (dB)')
 
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
+    for abr in data:
+        x = data[abr][0] * 100  # -> %
+        y = data[abr][1]
 
-    for abr_cc in ssim:
-        abr, cc = abr_cc
-
-        total_rebuf = rebuffer[abr_cc]['total_rebuf']
-        total_play = rebuffer[abr_cc]['total_play']
-        rebuf_rate = total_rebuf / total_play
-
-        x = rebuf_rate * 100  # %
-        y = ssim[abr_cc]
         ax.scatter(x, y, color=pretty_colors[abr], clip_on=False)
         ax.annotate(pretty_names[abr], (x, y))
 
@@ -45,81 +96,46 @@ def plot_ssim_rebuffer(ssim, rebuffer):
     ax.set_xlim(xmin, xmax)
     ax.invert_xaxis()
 
+    ax.set_xlabel('Time spent stalled (%)')
+    ax.set_ylabel('Average SSIM (dB)')
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
     fig.savefig(args.o, bbox_inches='tight')
     sys.stderr.write('Saved plot to {}\n'.format(args.o))
 
 
-def collect_ssim(influx_client):
-    video_acked_results = influx_client.query('SELECT * FROM video_acked')
-
-    x = {}
-
-    for pt in video_acked_results['video_acked']:
-        abr_cc = tuple(pt['expt_id'].split('+'))
-        if abr_cc not in x:
-            x[abr_cc] = []
-
-        ssim_index = get_ssim_index(pt)
-        if ssim_index is not None:
-            x[abr_cc].append(ssim_index)
-
-    # calculate average SSIM in dB
-    for abr_cc in x:
-        avg_ssim_index = np.mean(x[abr_cc])
-        avg_ssim_db = ssim_index_to_db(avg_ssim_index)
-        x[abr_cc] = avg_ssim_db
-
-    return x
-
-
-def calculate_rebuffer_by_abr_cc(d):
-    x = {}  # indexed by (abr, cc)
-
-    for session in d:
-        expt_id = session[-1]
-        abr_cc = tuple(expt_id.split('+'))
-
-        if abr_cc not in x:
-            x[abr_cc] = {}
-            x[abr_cc]['total_play'] = 0
-            x[abr_cc]['total_rebuf'] = 0
-
-        x[abr_cc]['total_play'] += d[session]['play']
-        x[abr_cc]['total_rebuf'] += d[session]['rebuf']
-
-    return x
-
-
-def collect_rebuffer(influx_client):
-    client_buffer_results = influx_client.query('SELECT * FROM client_buffer')
-
-    buffer_data = buffer_data_by_session(client_buffer_results)
-
-    rebuffer = calculate_rebuffer_by_abr_cc(buffer_data)
-
-    return rebuffer
-
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('yaml_settings')
+    parser.add_argument('yaml_settings', nargs='?')
     parser.add_argument('-o', required=True)
+    parser.add_argument('-i', help='input JSON data')
     global args
     args = parser.parse_args()
+
+    if args.i:
+        with open(args.i) as fh:
+            data = json.load(fh)
+            plot_ssim_rebuffer(data)
+        return
 
     with open(args.yaml_settings, 'r') as fh:
         yaml_settings = yaml.safe_load(fh)
 
     # create an InfluxDB client and perform queries
+    global influx_client
     influx_client = connect_to_influxdb(yaml_settings)
 
-    ssim = collect_ssim(influx_client)
-    rebuffer = collect_rebuffer(influx_client)
+    data = get_data()
 
-    print(ssim)
-    print(rebuffer)
+    # dump data to .json
+    json_file = path.splitext(args.o)[0] + '.json'
+    with open(json_file, 'w') as fh:
+        json.dump(data, fh)
+    sys.stderr.write('Dumped data to {}\n'.format(json_file))
 
-    plot_ssim_rebuffer(ssim, rebuffer)
+    plot_ssim_rebuffer(data)
 
 
 if __name__ == '__main__':
