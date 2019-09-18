b780e6d472e8f83272b40f12efb910d78c33288b
Chenzhi Zhu <mrbrtpt@gmail.com>
2019-09-18 17:16:40 +0800

    plot_pred_acc.py: enable to parallel computing

--- src/scripts/plot_pred_acc.py
+++ src/scripts/plot_pred_acc.py
@@ -12,7 +12,9 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-from helpers import connect_to_influxdb
+from multiprocessing import Pool
+
+from helpers import connect_to_influxdb, time_pair, datetime_iter_list
 from stream_processor import VideoStream
 from ttp import Model, PKT_BYTES
 from ttp2 import Model as Linear_Model
@@ -28,10 +30,6 @@ ABL_COMMON_PATH = '/home/ubuntu/models/puffer_abl/'
 PRINT_EVERY = 10000
 
 args = None
-expt = {}
-influx_client = None
-ttp_model = Model()
-linear_model = Linear_Model()
 
 abl_names = ['ttp_nh', 'ttp_nhd', 'ttp_nhci', 'ttp_nhmr']
 abl_settings = {'ttp_nh': (0, TCP_INFO),
@@ -42,28 +40,14 @@ abl_paths = {'ttp_nh': ABL_COMMON_PATH + 'abl_dcimr_0/py-0.pt',
              'ttp_nhd': ABL_COMMON_PATH + 'abl_cimr_0/py-0.pt',
              'ttp_nhci': ABL_COMMON_PATH + 'abl_dmr_0/py-0.pt',
              'ttp_nhmr': ABL_COMMON_PATH + 'abl_dci_0/py-0.pt'}
-abl_models = {name: Abl_Model(past_chunks=abl_settings[name][0],
-                              tcp_info=abl_settings[name][1])\
-                    for name in abl_names}
 
 terms = ['bin', 'l1', 'l2']
 predictors = ['ttp', 'ttp_mle', 'ttp_tp', 'linear', 'tcp_info', 'harmonic']\
              + abl_names
-result = {predictor: {term: 0 for term in terms} for predictor in predictors}
-
-tot = 0
 
 
-def process_session(session, s):
-    global tot
-    global result
-    global ttp_model
-    global linear_model
-    global expt
-    global terms
-    global predictors
-    global PAST_CHUNKS
-
+def process_session(session, s, tot, result, ttp_model, linear_model,
+                    abl_models, expt):
     for curr_ts in s:
         chunks = []
 
@@ -78,7 +62,7 @@ def process_session(session, s):
 
         curr_chunk_size = chunks[0]['size']
 
-        tot += 1
+        tot[0] += 1
         (raw_in, raw_out) = prepare_intput_output(chunks)
         unit_raw_in = copy.deepcopy(raw_in)
         unit_raw_in[0][-1] = MID_SIZE / PKT_BYTES
@@ -154,48 +138,82 @@ def process_session(session, s):
         result['harmonic']['l1'] += l1_loss(harm_out[0], raw_out[0])
         result['harmonic']['l2'] += l2_loss(harm_out[0], raw_out[0])
 
-        if tot % PRINT_EVERY == 0:
-            print('For tot:', tot)
-            for pred in predictors:
-                print(pred + ': ' + ', '.join([term + ':{:.5f}'.format(
-                          result[pred][term] / tot) for term in terms]))
+
+def worker(start_time, end_time):
+    tot = [0]
+    result = {predictor: {term: 0 for term in terms} for predictor in predictors}
+    abl_models = {name: Abl_Model(past_chunks=abl_settings[name][0],
+                    tcp_info=abl_settings[name][1])\
+                    for name in abl_names}
+    expt = None
+
+    ttp_model = Model()
+    linear_model = Linear_Model()
+
+
+    with open(args.yaml_settings, 'r') as fh:
+        yaml_settings = yaml.safe_load(fh)
+
+    with open(args.expt, 'r') as fh:
+        expt = json.load(fh)
+
+    # create an InfluxDB client and perform queries
+    influx_client = connect_to_influxdb(yaml_settings)
+
+    ttp_model.load(args.ttp_path)
+
+    linear_model.load("/home/ubuntu/models/puffer_ttp/linear/py-0.pt")
+
+    for name, model in abl_models.items():
+        model.load(abl_paths[name])
+
+    video_stream = VideoStream(
+            lambda session, s, tot=tot, result=result, ttp_model=ttp_model,
+                    linear_model=linear_model, abl_models=abl_models,
+                    expt=expt: process_session(session, s, tot, result,
+                         ttp_model, linear_model, abl_models, expt))
+    video_stream.process(influx_client, start_time, end_time)
+
+    return (tot[0], result)
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('yaml_settings')
-    parser.add_argument('--from', dest='start_time', required=True,
-                        help='datetime in UTC conforming to RFC3339')
-    parser.add_argument('--to', dest='end_time', required=True,
-                        help='datetime in UTC conforming to RFC3339')
+    parser.add_argument('-t', action='append', type=time_pair, required=True)
     parser.add_argument('--expt', help='e.g., expt_cache.json')
     parser.add_argument('-o', '--output', required=True)
     parser.add_argument('--ttp-path', dest='ttp_path', required=True)
     global args
     args = parser.parse_args()
 
-    with open(args.yaml_settings, 'r') as fh:
-        yaml_settings = yaml.safe_load(fh)
 
-    with open(args.expt, 'r') as fh:
-        global expt
-        expt = json.load(fh)
+    pool = Pool(processes=12)
+    procs = []
 
-    # create an InfluxDB client and perform queries
-    global influx_client
-    influx_client = connect_to_influxdb(yaml_settings)
+    for s_str, e_str in datetime_iter_list(args.t, 4):
+        procs.append((pool.apply_async(worker, (s_str, e_str,)),
+                      s_str, e_str))
 
-    global ttp_model
-    ttp_model.load(args.ttp_path)
+    tot = 0
+    result = {predictor: {term: 0 for term in terms} for predictor in predictors}
 
-    global linear_model
-    linear_model.load("/home/ubuntu/models/puffer_ttp/linear/py-0.pt")
+    for proc, s_str, e_str in procs:
+        _tot, _result = proc.get()
+        tot += _tot
+        for pred in predictors:
+            for term in terms:
+                result[pred][term] += _result[pred][term]
 
-    global abl_models
-    for name, model in abl_models.items():
-        model.load(abl_paths[name])
+        if tot == 0:
+            continue
 
-    video_stream = VideoStream(process_session)
-    video_stream.process(influx_client, args.start_time, args.end_time)
+        print('Processing video_sent and video_acked data '
+              'between {} and {}\n'.format(s_str, e_str))
+        print('For tot:', tot)
+        for pred in predictors:
+            print(pred + ': ' + ', '.join([term + ':{:.5f}'.format(
+              result[pred][term] / tot) for term in terms]))
 
     with open(args.output, 'w') as fh:
         for pred in predictors:
