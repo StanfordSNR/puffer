538d0f7ef2fed1ce8c241ac16882198ddd4b9223
Francis Y. Yan <fyy@cs.stanford.edu>
2021-03-04 06:25:09 +0000

    offline_test.py: get rid of SSL

--- src/media-server/offline_test.py
+++ src/media-server/offline_test.py
@@ -1,64 +1,68 @@
 #!/usr/bin/env python3
-import time
+
 import os
-import subprocess
+from os import path
+import time
 import signal
+import shutil
+from subprocess import check_call, Popen
 
 
-def run_offline_media_servers():
-    run_servers_cmd = './run_servers ../settings_offline.yml'
-    p1 = subprocess.Popen(run_servers_cmd, shell=True)
+def start_mahimahi_clients(num_clients):
+    # create a temporary directory to store Chrome sessions
+    chrome_sessions = "chrome-sessions"
+    if not path.exists(chrome_sessions):
+        os.makedirs(chrome_sessions)
 
-
-def start_maimahi_clients(num_clients):
+    # declare variable to store a list of processes
     plist = []
+
     try:
         trace_dir = "/home/ubuntu/fcc_mahimahi_traces"
-        #trace_dir = "/home/ubuntu/exact_train_traces_mm_fixed_fcc"
-        # To test nowrway traces use: /home/ubuntu/norway_traces"
+        # to test nowrway traces use: /home/ubuntu/norway_traces"
+
         files = os.listdir(trace_dir)
         for filename in files:
-            # mahimahi_cmd = 'mm-delay 40 mm-link 12mbps ' + trace_dir + '/' + \
-            #                filename
             base_port = 9361
-            remote_base_port = 9222
+            remote_base_port = 10000
             plist = []
-            for i in range(1, num_clients + 1):
-                remote_port = remote_base_port + i
-                port = base_port + i
-                #chrome_cmd = 'chromium-browser --headless --disable-gpu --remote-debugging-port=9222 ' + \
-                #             'http://$MAHIMAHI_BASE:8080/player/?wsport=' + \
-                #             str(port) + ' --user-data-dir=./' + str(port) + \
-                #             '.profile'
-                time.sleep(4)
-                mahimahi_chrome_cmd = "mm-delay 40 mm-link 12mbps {}/{} -- sh -c 'chromium-browser --headless --disable-gpu --remote-debugging-port={} https://$MAHIMAHI_BASE/player/?wsport={} --user-data-dir=./{}.profile'".format(trace_dir, filename, remote_port, port, port)
-                print(mahimahi_chrome_cmd)
-                chrome_cmd_b = mahimahi_chrome_cmd.encode('utf-8')
-                p = subprocess.Popen(mahimahi_chrome_cmd, shell=True,
-                                     preexec_fn=os.setsid)
-                plist.append(p)
 
-            time.sleep(60*8)
+            for i in range(1, num_clients + 1):
+                port = base_port + i
+                remote_debugging_port = remote_base_port + i
+
+                # downlink: trace_dir/TRACE (to transmit video)
+                # uplink: 12 Mbps (fast enough for ack messages)
+                mahimahi_chrome_cmd = "mm-delay 40 mm-link 12mbps {}/{} -- sh -c 'chromium-browser --headless --disable-gpu --remote-debugging-port={} http://$MAHIMAHI_BASE:8080/player/?wsport={} --user-data-dir={}/{}.profile'".format(trace_dir, filename, remote_debugging_port, port, chrome_sessions, port)
+                print(mahimahi_chrome_cmd)
+
+                p = Popen(mahimahi_chrome_cmd.encode('utf-8'),
+                          shell=True, preexec_fn=os.setsid)
+                plist.append(p)
+                time.sleep(4)  # don't know why but seems necessary
+
+            time.sleep(60 * 8)
+
             for p in plist:
                 os.killpg(os.getpgid(p.pid), signal.SIGTERM)
                 time.sleep(4)
 
-            subprocess.check_call("rm -rf ./*.profile", shell=True,
-                                  executable='/bin/bash')
+            shutil.rmtree(chrome_sessions, ignore_errors=True)
     except Exception as e:
         print("exception: " + str(e))
         pass
     finally:
         for p in plist:
             os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-            subprocess.check_call("rm -rf ./*.profile", shell=True,
-                                  executable='/bin/bash')
+            shutil.rmtree(chrome_sessions, ignore_errors=True)
 
 
 def main():
-    subprocess.check_call('sudo sysctl -w net.ipv4.ip_forward=1', shell=True)
-    #run_offline_media_servers()
-    start_maimahi_clients(5)
+    # enable IP forwarding
+    check_call('sudo sysctl -w net.ipv4.ip_forward=1', shell=True)
+
+    # assume web server and media server are both running
+    start_mahimahi_clients(5)
 
 
 if __name__ == '__main__':
